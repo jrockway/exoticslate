d752b9fabd00c105b2ef71695526c7d0cc0b1e10
Chris Dent <chris.dent@socialtext.com>
2007-08-02 21:05:45 +0000

    update version number in preparation to tag a release
    git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/trunk@4217 593ce9bc-2028-0410-bb98-c566b643a948

--- nlw/lib/Socialtext.pm
+++ nlw/lib/Socialtext.pm
@@ -15,7 +15,7 @@ use Socialtext::Authz;
 use Socialtext::Permission 'ST_READ_PERM';
 use Socialtext::Validate qw( validate SCALAR_TYPE USER_TYPE WORKSPACE_TYPE );
 
-our $VERSION = '2.14.3.0';
+our $VERSION = '2.14.4.0';
 
 const product_version => $VERSION;
 field using_debug => 0;

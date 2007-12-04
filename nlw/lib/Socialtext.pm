113ebb3511af0867dd6717516980ca2075a9a4e4
Jon Prettyman <jon.prettyman@socialtext.com>
2007-12-04 17:02:24 +0000

    r26762@milton (orig r5911):  jjp | 2007-12-04 08:47:22 -0800  preparing version for release
    git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/trunk@5926 593ce9bc-2028-0410-bb98-c566b643a948

--- nlw/lib/Socialtext.pm
+++ nlw/lib/Socialtext.pm
@@ -15,7 +15,7 @@ use Socialtext::Authz;
 use Socialtext::Permission 'ST_READ_PERM';
 use Socialtext::Validate qw( validate SCALAR_TYPE USER_TYPE WORKSPACE_TYPE );
 
-our $VERSION = '2.17.3.0';
+our $VERSION = '2.18.0.0';
 
 const product_version => $VERSION;
 field using_debug => 0;

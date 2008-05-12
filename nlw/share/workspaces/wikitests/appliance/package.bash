9945fd454f67bf9183e903b0704df9ccd0449f37
Ken Pier <ken.pier@socialtext.com>
2008-05-12 18:20:46 +0000

    Refinements to the new DEB package for appliance wikitests
    git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/trunk@7415 593ce9bc-2028-0410-bb98-c566b643a948

--- nlw/share/workspaces/wikitests/appliance/package.bash
+++ nlw/share/workspaces/wikitests/appliance/package.bash
@@ -1,3 +1,20 @@
 #!/bin/sh
 # @COPYRIGHT@
-dir2deb --dir ./ --p wikitests --root ~ --description Packages up wikitests --file-perms 777
+
+# Create a tarball from the latest wikitest files
+
+if [ -e $ST_CURRENT/nlw/share/workspaces/wikitests/wikitests.1.tar.gz ]; then
+    echo DELETING old wikitests.1.tar.gz
+    rm $ST_CURRENT/nlw/share/workspaces/wikitests/wikitests.1.tar.gz
+fi
+
+echo CREATING new wikitests.1.tar.gz
+$ST_CURRENT/nlw/bin/st-admin export-workspace --w wikitests --dir $ST_CURRENT/nlw/share/workspaces/wikitests/
+cp $ST_CURRENT/nlw/share/workspaces/wikitests/wikitests.1.tar.gz $ST_CURRENT/nlw/share/workspaces/wikitests/appliance
+
+echo PACKAGING wikitests using dir2deb
+dir2deb --dir $ST_CURRENT/nlw/share/workspaces/wikitests/appliance  --p wikitests  --description Package wikitests for appliances -X package.bash -X appliance-wikitests.tar.gz -X ".svn" --strip-outer-dir
+
+echo CREATING new appliance-wikitests.tar.gz
+tar zcf appliance-wikitests.tar.gz --exclude=".svn"  --exclude="*.deb" --exclude=appliance-wikitests.tar.gz --exclude="package.bash" *
+

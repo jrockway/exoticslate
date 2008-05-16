d567828473656be8081ef47b4eb8cefbdfd179bb
Ken Pier <ken.pier@socialtext.com>
2008-05-16 23:17:02 +0000

    Revised to use dpkg.  Programs are in /usr/bin, data and source scripts are in /usr/share/nlw/wikitests
    git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/trunk@7528 593ce9bc-2028-0410-bb98-c566b643a948

--- nlw/share/workspaces/wikitests/appliance/package.bash
+++ nlw/share/workspaces/wikitests/appliance/package.bash
@@ -12,9 +12,14 @@ echo CREATING new wikitests.1.tar.gz
 $ST_CURRENT/nlw/bin/st-admin export-workspace --w wikitests --dir $ST_CURRENT/nlw/share/workspaces/wikitests/
 cp $ST_CURRENT/nlw/share/workspaces/wikitests/wikitests.1.tar.gz $ST_CURRENT/nlw/share/workspaces/wikitests/appliance
 
-echo PACKAGING wikitests using dir2deb
-dir2deb --dir $ST_CURRENT/nlw/share/workspaces/wikitests/appliance  --p wikitests  --description Package wikitests for appliances -X package.bash -X appliance-wikitests.tar.gz -X ".svn" --strip-outer-dir
+echo COPYING FILES FOR PACKAGING
+mkdir -p usr/bin
+cp  create-test-data-workspace fetch-test-data-tarball prep-wikitests run-wiki-tests usr/bin/
 
-echo CREATING new appliance-wikitests.tar.gz
-tar zcf appliance-wikitests.tar.gz --exclude=".svn"  --exclude="*.deb" --exclude=appliance-wikitests.tar.gz --exclude="package.bash" *
+mkdir -p usr/share/nlw/wikitests
+cp  do-wiki-tests  one-wiki-test  README  set-time  setup-selenium  wikitestfiles.zip  wikitests.1.tar.gz usr/share/nlw/wikitests/
+
+echo PACKAGING wikitests using dir2deb
+rm -f *.deb
+dir2deb --dir $ST_CURRENT/nlw/share/workspaces/wikitests/appliance/usr  --package  wikitests  --description Package wikitests for appliances -X package.bash -X appliance-wikitests.tar.gz -X ".svn" 
 

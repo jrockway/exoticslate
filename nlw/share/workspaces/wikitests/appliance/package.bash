5fc9df84f53da4786b54bcc7c4e6dd1f9d2eba81
Ken Pier <ken.pier@socialtext.com>
2008-05-16 23:50:55 +0000

    Added Socialcalc calctests to package
    git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/trunk@7529 593ce9bc-2028-0410-bb98-c566b643a948

--- nlw/share/workspaces/wikitests/appliance/package.bash
+++ nlw/share/workspaces/wikitests/appliance/package.bash
@@ -1,7 +1,7 @@
 #!/bin/sh
 # @COPYRIGHT@
 
-# Create a tarball from the latest wikitest files
+# Create  tarballs from the latest wikitest files
 
 if [ -e $ST_CURRENT/nlw/share/workspaces/wikitests/wikitests.1.tar.gz ]; then
     echo DELETING old wikitests.1.tar.gz
@@ -12,14 +12,23 @@ echo CREATING new wikitests.1.tar.gz
 $ST_CURRENT/nlw/bin/st-admin export-workspace --w wikitests --dir $ST_CURRENT/nlw/share/workspaces/wikitests/
 cp $ST_CURRENT/nlw/share/workspaces/wikitests/wikitests.1.tar.gz $ST_CURRENT/nlw/share/workspaces/wikitests/appliance
 
+if [ -e $ST_CURRENT/nlw/share/workspaces/calctests/calctests.1.tar.gz ]; then
+    echo DELETING old calctests.1.tar.gz
+    rm $ST_CURRENT/nlw/share/workspaces/calctests/calctests.1.tar.gz
+fi
+
+echo CREATING new calctests.1.tar.gz
+$ST_CURRENT/nlw/bin/st-admin export-workspace --w calctests --dir $ST_CURRENT/nlw/share/workspaces/calctests/
+cp $ST_CURRENT/nlw/share/workspaces/calctests/calctests.1.tar.gz $ST_CURRENT/nlw/share/workspaces/wikitests/appliance
+
 echo COPYING FILES FOR PACKAGING
 mkdir -p usr/bin
 cp  create-test-data-workspace fetch-test-data-tarball prep-wikitests run-wiki-tests usr/bin/
 
 mkdir -p usr/share/nlw/wikitests
-cp  do-wiki-tests  one-wiki-test  README  set-time  setup-selenium  wikitestfiles.zip  wikitests.1.tar.gz usr/share/nlw/wikitests/
+cp  do-wiki-tests  one-wiki-test  README  set-time  setup-selenium  wikitestfiles.zip  wikitests.1.tar.gz calctests.1.tar.gz usr/share/nlw/wikitests/
 
 echo PACKAGING wikitests using dir2deb
-rm -f *.deb
+rm -f wikitests*.deb
 dir2deb --dir $ST_CURRENT/nlw/share/workspaces/wikitests/appliance/usr  --package  wikitests  --description Package wikitests for appliances -X package.bash -X appliance-wikitests.tar.gz -X ".svn" 
 

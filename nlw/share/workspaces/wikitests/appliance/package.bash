b3bc983d13897b9746062857806a3aee55b87e36
Ken Pier <ken.pier@socialtext.com>
2008-06-22 03:05:23 +0000

    Add st-enable-ldap capability
    git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/trunk@7993 593ce9bc-2028-0410-bb98-c566b643a948

--- nlw/share/workspaces/wikitests/appliance/package.bash
+++ nlw/share/workspaces/wikitests/appliance/package.bash
@@ -45,10 +45,10 @@ rm -rf $ST_CURRENT/nlw/share/workspaces/wikitests/appliance/usr
 
 echo COPYING FILES FOR PACKAGING
 mkdir -p $ST_CURRENT/nlw/share/workspaces/wikitests/appliance/usr/bin
-cp   st-socialcalc prep-wikitests run-wiki-tests $ST_CURRENT/nlw/share/workspaces/wikitests/appliance/usr/bin/
+cp st-enable-ldap  st-socialcalc prep-wikitests run-wiki-tests $ST_CURRENT/nlw/share/workspaces/wikitests/appliance/usr/bin/
 
 mkdir -p $ST_CURRENT/nlw/share/workspaces/wikitests/appliance/usr/share/nlw/wikitests
-cp  do-tests do-calc-tests do-wiki-tests  one-wiki-test  one-calc-test README  set-time  setup-selenium  test-data.tar.gz wikitestfiles.zip  wikitests.1.tar.gz calctests.1.tar.gz $ST_CURRENT/nlw/share/workspaces/wikitests/appliance/usr/share/nlw/wikitests/
+cp ldap.yaml.st do-tests do-calc-tests do-wiki-tests  one-wiki-test  one-calc-test README  set-time  setup-selenium  test-data.tar.gz wikitestfiles.zip  wikitests.1.tar.gz calctests.1.tar.gz $ST_CURRENT/nlw/share/workspaces/wikitests/appliance/usr/share/nlw/wikitests/
 
 echo PACKAGING wikitests using dir2deb
 rm -f $ST_CURRENT/nlw/share/workspaces/wikitests/appliance/wikitests*.deb

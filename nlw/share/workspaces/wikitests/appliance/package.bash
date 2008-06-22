bb7946861c065db2204430affa95bd89d6015125
Ken Pier <ken.pier@socialtext.com>
2008-06-22 03:57:56 +0000

    Using st-ldap instead of st-enable-ldap
    git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/trunk@7998 593ce9bc-2028-0410-bb98-c566b643a948

--- nlw/share/workspaces/wikitests/appliance/package.bash
+++ nlw/share/workspaces/wikitests/appliance/package.bash
@@ -45,7 +45,7 @@ rm -rf $ST_CURRENT/nlw/share/workspaces/wikitests/appliance/usr
 
 echo COPYING FILES FOR PACKAGING
 mkdir -p $ST_CURRENT/nlw/share/workspaces/wikitests/appliance/usr/bin
-cp st-enable-ldap  st-socialcalc prep-wikitests run-wiki-tests $ST_CURRENT/nlw/share/workspaces/wikitests/appliance/usr/bin/
+cp st-ldap  st-socialcalc prep-wikitests run-wiki-tests $ST_CURRENT/nlw/share/workspaces/wikitests/appliance/usr/bin/
 
 mkdir -p $ST_CURRENT/nlw/share/workspaces/wikitests/appliance/usr/share/nlw/wikitests
 cp ldap.yaml.st do-tests do-calc-tests do-wiki-tests  one-wiki-test  one-calc-test README  set-time  setup-selenium  test-data.tar.gz wikitestfiles.zip  wikitests.1.tar.gz calctests.1.tar.gz $ST_CURRENT/nlw/share/workspaces/wikitests/appliance/usr/share/nlw/wikitests/

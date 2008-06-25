36227ae5686c2de179681693553805aa9d0d75d6
Jeremy Stashewsky <jeremy.stashewsky@socialtext.com>
2008-06-25 22:40:39 +0000

    Make get_events understand params TDD style, yo!
    git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/trunk@8067 593ce9bc-2028-0410-bb98-c566b643a948

--- nlw/lib/Socialtext/Events/Reporter.pm
+++ nlw/lib/Socialtext/Events/Reporter.pm
@@ -10,12 +10,56 @@ sub new {
     return bless {}, $class;
 }
 
+=head2 get_events()
+
+Return an arrayref of events matching the specified criteria.
+
+Parameters:
+
+=over 4
+
+=item before - Any events from before this timestamp
+
+=item after - Any events from after this timestamp
+
+=item action - Any events matching this action
+
+=item limit - Return this number of events at most
+
+=back
+
+=cut
+
 sub get_events {
     my $self = shift;
     my %opts = @_;
 
-    my $sth = sql_execute(<<EOSQL);
+    my @args;
+    my $where = '';
+    if (my $b = $opts{before}) {
+        $where = q{WHERE timestamp < '?'::timestamptz};
+        push @args, $b;
+    }
+    elsif (my $a = $opts{after}) {
+        $where = q{WHERE timestamp > '?'::timestamptz};
+        push @args, $a;
+    }
+    if (my $a = $opts{action}) {
+        $where .= $where ? ' AND ' : 'WHERE ';
+        $where .= 'action = ?';
+        push @args, $a;
+    }
+
+    my $limit = '';
+    if (my $l = $opts{limit} || $opts{count}) {
+        $limit = 'LIMIT ?';
+        push @args, $l;
+    }
+
+    my $sth = sql_execute(<<EOSQL, @args);
 SELECT * FROM event
+    $where
+    $limit
 EOSQL
     return $sth->fetchall_arrayref;
 }

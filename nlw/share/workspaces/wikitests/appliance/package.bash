e85562afbac5a8e7ad4c8003f599c8a1c46182f1
Ken Pier <ken.pier@socialtext.com>
2008-07-03 06:07:01 +0000

    Added st-bootstrap-openldap to the package
    git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/trunk@8181 593ce9bc-2028-0410-bb98-c566b643a948

--- nlw/share/workspaces/wikitests/appliance/package.bash
+++ nlw/share/workspaces/wikitests/appliance/package.bash
@@ -30,7 +30,8 @@ rm -rf $ST_CURRENT/nlw/share/workspaces/wikitests/appliance/usr
 echo COPYING FILES FOR PACKAGING
 mkdir -p $ST_CURRENT/nlw/share/workspaces/wikitests/appliance/usr/bin
 RWT=`which run-wiki-tests`
-cp $RWT st-ldap  st-socialcalc prep-wikitests  $ST_CURRENT/nlw/share/workspaces/wikitests/appliance/usr/bin/
+STB=`which st-bootstrap-openldap`
+cp $STB $RWT st-ldap  st-socialcalc prep-wikitests  $ST_CURRENT/nlw/share/workspaces/wikitests/appliance/usr/bin/
 
 mkdir -p $ST_CURRENT/nlw/share/workspaces/wikitests/appliance/usr/share/nlw/wikitests
 cp ldap.yaml.st do-tests do-calc-tests do-wiki-tests  one-wiki-test aliases README  set-time  setup-selenium  test-data.tar.gz wikitestfiles.zip  wikitests.1.tar.gz $ST_CURRENT/nlw/share/workspaces/wikitests/appliance/usr/share/nlw/wikitests/

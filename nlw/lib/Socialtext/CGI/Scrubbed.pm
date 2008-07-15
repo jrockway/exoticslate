10cc819623d9bb8fd8bc8bfa799ea21a01d974ed
Ingy Dot Net <ingy.dot.net@socialtext.com>
2008-07-15 19:39:56 +0000

    Apply the fix for {bz: 791} to trunk.
    git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/trunk@8429 593ce9bc-2028-0410-bb98-c566b643a948

--- nlw/lib/Socialtext/CGI/Scrubbed.pm
+++ nlw/lib/Socialtext/CGI/Scrubbed.pm
@@ -8,7 +8,7 @@ use HTML::Scrubber;
 
 field 'scrubber', -init => 'HTML::Scrubber->new';
 
-my %dont_scrub = map { $_ => 1 } qw(page_body content);
+my %dont_scrub = map { $_ => 1 } qw(page_body content comment);
 
 sub param {
     my $self = shift;

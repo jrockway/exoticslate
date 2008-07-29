a5d99875d385f4d85c5d31a35f8452c2ac337e49
Gabe Wachob <gabe.wachob@socialtext.com>
2008-07-29 18:20:14 +0000

    Added perldoc and refactored a bit to make using logical_name consistent throughout
    git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/trunk@8641 593ce9bc-2028-0410-bb98-c566b643a948

--- nlw/lib/Socialtext/WikiFixture/SocialWidgets.pm
+++ nlw/lib/Socialtext/WikiFixture/SocialWidgets.pm
@@ -4,18 +4,58 @@ use strict;
 use warnings;
 use base 'Socialtext::WikiFixture::Socialtext';
 use Test::More;
+=head1 NAME
+
+Socialtext::WikiFixture::SocialWidgets - Test the Widgets using Selenium
+
+=cut
 
 our $VERSION = '0.01';
+
+=head1 DESCRIPTION
+
+This module is a subclass of Socialtext::WikiFixture::Socialtext and includes
+extra commands specifically for testing the Socialtext Widgets (gadgets) containers.
+
+=head1 FUNCTIONS
+
+=head2 new( %opts )
+
+Create a new fixture object. The same options as 
+Socialtext::WikiFixture::Socialtext are required except that no workspace is required:
+
+=over 4
+
+=item username
+
+Mandatory - the username to login to the wiki with.
+
+=item password 
+
+Mandatory - the password to login to the wiki with.
+
+=back
+
+=head2 init()
+
+Initializes the object, and logs into the Socialtext server.
+
+=cut
 
 sub init {
     my ($self) = @_;
     $self->{mandatory_args} = [qw(username password)];
     $self->{workspace}="";
     $self->{_widgets}={};
-    # Also $self->{_curwidget}
     $self->SUPER::init;
 }
 
+=head2 st_empty_container ( )
+
+Empties the current container using the action=clear_widgets parameter. 
+You should navigate to the container URL using normal Selenese test command like "open"
+
+=cut
 sub st_empty_container { 
     my ($self) = @_;
     $self->{selenium}->open_ok("?action=clear_widgets");
@@ -24,11 +64,29 @@ sub st_empty_container {
     diag "Widgets: $widgetlist\n"; 
 }
 
+=head2 st_reset_container ( )
+
+Resets the current container to default contehnts using the action=reset_container parameter. 
+You should navigate to the container URL using normal Selenese test command like "open"
+
+=cut
 sub st_reset_container {
     my ($self) = @_;
     $self->{selenium}->open_ok("?action=reset_container");
     $self->{selenium}->wait_for_page_to_load_ok(10000);
 }
+
+=head2 st_add_widget ( widgetpath, logical_name )
+
+Adds a widget to the container. The widget is identified with the widgetpath parameter
+and is the same value that is used by the file parameter in the add_widget action.
+
+The logical_name parameter is the logical name that is assigned to this instance of the
+widget. All future references to this widget will be made using this logical name. 
+
+3rd Party Hosted widgets are not yet supported.
+
+=cut
 
 sub st_add_widget {
     my ($self, $widgetpath, $logical) = @_;
@@ -42,6 +100,18 @@ sub st_add_widget {
     diag "Named this widget '$logical': ".$self->{_widgets}{$logical}."\n";
 }
 
+=head2 st_name_widget ( position, logical_name )
+
+Assigns a name to the widget in the container which is at the given position. This
+is useful for precooked containers where specific widgets are always placed in a specific
+order. The position parameter is used to match against the part of the widget id after
+the underscore (e.g. "5" would match the widget with id 2349038923048320_5). 
+
+The logical_name parameter is the logical name that is assigned to this instance of the
+widget. All future references to this widget will be made using this logical name. 
+
+=cut
+
 sub st_name_widget {
     my ($self, $position, $logical) = @_;
     my @widgetlist = $self->_getWidgetList;
@@ -49,11 +119,28 @@ sub st_name_widget {
     diag "Named this widget '$logical': ".$self->{_widgets}{$logical}."\n";
 }
 
+=head2 st_minimize_widget ( logical_name )
+
+This clicks on the minimize button for the widget with the logical name given. This
+also "restores" the widget to original size if the widget is already minimized.
+
+This assumes that the currently selected frame is the "parent" container frame.
+
+=cut
+
 sub st_minimize_widget {
     my ($self, $logical) = @_;
     my $widget = $self->{_widgets}{$logical};
     $self->{selenium}->click("xpath=//div[\@id='$widget']//img[\@id='st-dashboard-minimize']");
 }
+
+=head2 st_remove_widget ( logical_name )
+
+This clicks on the remove button for the widget with the logical name given. 
+
+This assumes that the currently selected frame is the "parent" container frame.
+
+=cut
 
 sub st_remove_widget {
     my ($self, $logical) = @_;
@@ -65,33 +152,75 @@ sub st_remove_widget {
     delete($self->{_widgets}{$logical});
 }
 
+=head2 st_widget_settings ( logical_name )
+
+This clicks on the settings button for the widget with the logical name given. 
+
+This assumes that the currently selected frame is the "parent" container frame.
+
+=cut
 sub st_widget_settings {
     my ($self, $logical) = @_;
     my $widget = $self->{_widgets}{$logical};
     $self->{selenium}->click("xpath=//div[\@id='$widget']//img[\@id='st-dashboard-settings']");
 }
 
-sub st_set_current_widget {
-    my ($self, $logical) = @_;
-    $self->{_curwidget}=$logical;
-}
-    
+=head2 st_widget_title_like ( logical_name )
+
+This performs a regex text match on the title of the widget (outside the iframe) with the 
+given logical name.
+
+This assumes that the currently selected frame is the "parent" container frame.
+
+=cut
+
 sub st_widget_title_like {
-    my ($self, $opt1) = @_;
-    $self->{selenium}->text_like("//span[\@class='gadget_title' and \@id='".$self->{_widgets}{$self->{_curwidget}}."-title-text']", $opt1);
+    my ($self, $logical, $opt1) = @_;
+    $self->{selenium}->text_like("//span[\@class='gadget_title' and \@id='".$self->{_widgets}{$logical}."-title-text']", $opt1);
 }
 
+=head2 st_widget_body_like ( logical_name )
+
+This performs a regex text match on the body (body element inside the iframe) of the widget 
+with the given logical name.
+
+This assumes that the currently selected frame is the "parent" container frame.
+
+=cut
 sub st_widget_body_like {
-    my ($self, $opt1) = @_;
-    $self->{selenium}->select_frame('xpath=//iframe[@id="'.$self->{_widgets}{$self->{_curwidget}}.'-iframe"]');
+    my ($self, $logical, $opt1) = @_;
+    $self->{selenium}->select_frame('xpath=//iframe[@id="'.$self->{_widgets}{$logical}.'-iframe"]');
     $self->{selenium}->text_like('//body', $opt1);
     $self->{selenium}->select_frame("relative=parent");
 }
+
+=head2 st_select_widget_frame ( logical_name )
+
+This sets the current frame to the one containing the widget with the logical name given.
+
+It operates like select_frame, and will allow the full set of selenium commands to operate 
+within the context of the widget iframe content, rather than the container. 
+
+Note that for other commands in Socialtext::WikiFixture::SocialWidgets to work, a test 
+script should call select-frame("reletive=parent") after invoking this to select the
+parent (default) container frame.
+
+=cut
 
 sub st_select_widget_frame {
     my ($self, $logical) = @_;
     $self->{selenium}->select_frame('xpath=//iframe[@id="'.$self->{_widgets}{$logical}.'-iframe"]');
 }
+
+=head2 st_wait_for_widget_load (logical_name, timeout )
+
+Waits for a widget's contents to finish loading, waiting for up to timeout milliseconds. Timeout 
+defaults to 10000 (10 seconds).
+
+Note that this command only waits until a widgets' html and javascript are loaded completely - 
+it does not wait for the widget to complete any behavior it is programmed to perform after loading.
+
+=cut
 
 sub st_wait_for_widget_load {
     my ($self, $logical, $timeout) = @_;
@@ -104,6 +233,16 @@ sub st_wait_for_widget_load {
 ENDJS
     $self->{selenium}->wait_for_condition_ok($js, $timeout);
 }
+
+=head2 st_wait_for_all_widgets_load (logical_name, timeout )
+
+Waits for all widgets in the container to finish loading, waiting for up to timeout milliseconds. 
+Timeout defaults to 10000 (10 seconds).
+
+Note that this command only waits until a widgets' html and javascript are loaded completely - 
+it does not wait for the widget to complete any behavior it is programmed to perform after loading.
+
+=cut
 
 sub st_wait_for_all_widgets_load {
     my ($self, $timeout) = @_;
@@ -137,3 +276,56 @@ sub _listDiff {
     }
     return @result;
 }
+
+=head1 AUTHOR
+
+Gabe Wachob, C<< <gabe.wachob at socialtext.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to
+C<bug-socialtext-editpage at rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Socialtext-WikiTest>.
+I will be notified, and then you'll automatically be notified of progress on
+your bug as I make changes.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Socialtext::WikiFixture::SocialWidgets
+
+You can also look for information at:
+
+=over 4
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Socialtext-WikiTest>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Socialtext-WikiTest>
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Socialtext-WikiTest>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Socialtext-WikiTest>
+
+=back
+
+=head1 ACKNOWLEDGEMENTS
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2008 Socialtext, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
+
+

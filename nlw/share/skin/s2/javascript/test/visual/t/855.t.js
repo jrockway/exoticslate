e0b0bee3c22a1e002bb2af03afec0f9a671f770b
Ingy Dot Net <ingy.dot.net@socialtext.com>
2008-08-14 02:12:19 +0000

    For IE, the elements in question ({bz: 855}) are not overlapping but their dimension and position are still not proper. Need to write other test script to test IE.
    git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/trunk@9044 593ce9bc-2028-0410-bb98-c566b643a948

--- nlw/share/skin/s2/javascript/test/visual/t/855.t.js
+++ nlw/share/skin/s2/javascript/test/visual/t/855.t.js
@@ -1,19 +1,23 @@
 var t = new Test.Visual();
 t.plan(1);
+if (jQuery.browser.gecko) {
+    t.open(
+        "/admin/index.cgi?action=recent_changes",
+        function(t) {
+            jQuery(t.iframe).height(200);
+            jQuery(t.iframe).width(800);
 
-t.open(
-    "/admin/index.cgi?action=recent_changes",
-    function(t) {
-        jQuery(t.iframe).height(200);
-        jQuery(t.iframe).width(800);
+            t.elements_do_not_overlap(
+                'div.tableFilter ul',
+                'div#controlsRight',
+                'Export and Tools do not overlap when window width is ' + 800
+            );
 
-        t.elements_do_not_overlap(
-            'div.tableFilter ul',
-            'div#controlsRight',
-            'Export and Tools do not overlap when window width is ' + 800
-        );
-
-        t.iframe.contentWindow.scrollTo(800, 50);
-    }
-);
+            t.iframe.contentWindow.scrollTo(800, 50);
+        }
+    );
+}
+else {
+    t.skip("This test is only for Mozilla browsers. On IE, those two elements are not overlapping but still look bad in other ways. ");
+}
 

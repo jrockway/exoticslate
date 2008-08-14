ede2a44f17963ca8379198506eebfa0ade412225
Ingy Dot Net <ingy.dot.net@socialtext.com>
2008-08-14 07:09:58 +0000

    Refactor test 855 and the Test.Visual class.
    Make them not invade the global namespace.
    
    Make the test file cleaner and tighter.
    
    git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/trunk@9050 593ce9bc-2028-0410-bb98-c566b643a948

--- nlw/share/skin/s2/javascript/test/visual/Test/Visual.js
+++ nlw/share/skin/s2/javascript/test/visual/Test/Visual.js
@@ -1,34 +1,32 @@
-proto = new Subclass('Test.Visual', 'Test.Base');
+(function($) {
 
-proto.doc = window.document;
+proto = new Subclass('Test.Visual', 'Test.Base');
 
 proto.init = function() {
     Test.Base.prototype.init.call(this);
     this.block_class = 'Test.Visual.Block';
+    this.doc = window.document;
 }
 
-proto.open = function(url, cb) {
+proto.open_iframe = function(url) {
     var asyncId = this.builder.beginAsync(60000);
 
-    this.iframe = jQuery("<iframe />").prependTo("body").get(0);
+    this.iframe = $("<iframe />").prependTo("body").get(0);
     this.iframe.contentWindow.location = url;
 
     var self = this;
-    jQuery(this.iframe).bind("load", function() {
+    $(this.iframe).bind("load", function() {
         self.doc = self.iframe.contentDocument;
-        cb.apply(self, [self]);
+        if (self.runTests)
+            self.runTests.apply(self, [self]);
 
         self.builder.endAsync(asyncId);
     });
 }
 
-// Test functions go here...
-proto.elements_do_not_overlap = function(selector1, selector2, reason) {
-    var element1 = this._get_selector_element(selector1);
-    var element2 = this._get_selector_element(selector2);
-
-    var $e1 = jQuery(element1);
-    var $e2 = jQuery(element2);
+proto.elements_do_not_overlap = function(selector1, selector2, name) {
+    var $e1 = $(this._get_selector_element(selector1));
+    var $e2 = $(this._get_selector_element(selector2));
 
     var off1 = $e1.offset();
     var off2 = $e2.offset();
@@ -37,20 +35,20 @@ proto.elements_do_not_overlap = function(selector1, selector2, reason) {
         (off1.top + $e1.height() > off2.top) &&
         (off1.left + $e1.width() > off2.left)
         ) {
-        this.fail(reason);
+        this.fail(name);
         return;
     }
 
-    this.pass(reason);
+    this.pass(name);
 }
 
 proto._get_selector_element = function(selector) {
-    var $result = jQuery(selector, this.doc);
+    var $result = $(selector, this.doc);
     if ($result.length <= 0)
-        throw("Nothing found for selector: '" + selector +"'");
+        throw("Nothing found for selector: '" + selector + "'");
     if ($result.length >= 2) {
         throw(String($result.length) + " elements found for selector: '" +
-            selector +"'"
+            selector + "'"
         );
     }
     return $result.get(0);
@@ -66,3 +64,5 @@ proto.init = function() {
 proto = new Subclass('Test.Visual.Filter', 'Test.Base.Filter');
 
 // Filter functions go here...
+
+})(jQuery);
--- nlw/share/skin/s2/javascript/test/visual/t/855.t.js
+++ nlw/share/skin/s2/javascript/test/visual/t/855.t.js
@@ -1,23 +1,29 @@
+(function($) {
+
 var t = new Test.Visual();
-t.plan(1);
-if (jQuery.browser.mozilla) {
-    t.open(
-        "/admin/index.cgi?action=recent_changes",
-        function(t) {
-            jQuery(t.iframe).height(200);
-            jQuery(t.iframe).width(800);
 
-            t.elements_do_not_overlap(
-                'div.tableFilter ul',
-                'div#controlsRight',
-                'Export and Tools do not overlap when window width is ' + 800
-            );
+t.plan(2);
 
-            t.iframe.contentWindow.scrollTo(800, 50);
-        }
-    );
-}
-else {
-    t.skip("This test is only for Mozilla browsers. On IE, those two elements are not overlapping but still look bad in other ways. ");
-}
+if (jQuery.browser.msie)
+    t.skipAll("{bz: 855} isa bug on Mozilla browsers. On IE, those two elements are not overlapping but still look bad in other ways.");
 
+t.runTests = function() {
+    $(t.iframe).height(200);
+    var widths = [1100, 800];
+    for (var i = 0, l = widths.length; i < l; i++) {
+        var width = widths[i];
+        $(t.iframe).width(width);
+
+        t.elements_do_not_overlap(
+            'div.tableFilter ul',
+            'div#controlsRight',
+            'Export and Tools do not overlap when window width is ' + width
+        );
+
+        t.iframe.contentWindow.scrollTo(width, 50);
+    }
+};
+
+t.open_iframe("/admin/index.cgi?action=recent_changes");
+
+})(jQuery);

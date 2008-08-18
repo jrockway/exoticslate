733ccf4030c7c5c22de9fd42a9c21b7fa2cf2175
Kang-min Liu <kang-min.liu@socialtext.com>
2008-08-18 22:10:20 +0000

    Add page attribution at the bottom of the page, with proper CSS to make them look OK.  Add corresponding visual test to make sure the page attribution texts are there.
    git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/trunk@9154 593ce9bc-2028-0410-bb98-c566b643a948

---
+++ nlw/share/skin/s2/javascript/test/visual/t/page-attribution.t.js
@@ -0,0 +1,30 @@
+(function($) {
+
+var t = new Test.Visual();
+
+t.plan(3);
+
+t.runTests = function() {
+    t.like(
+        t.$("#bottomButtons ul.buttonRight").text(),
+        /Created by/,
+        "There is page creator info at the bottom of the page"
+    );
+    t.like(
+        t.$("#bottomButtons ul.buttonRight").text(),
+        /Updated by/,
+        "There is page updator info at the bottom of the page"
+    );
+    t.like(
+        t.$("#bottomButtons ul.buttonRight").text(),
+        /\(\d+ revisions?\)/,
+        "There is a page revision info inside of a pair of parens at the bottom of the page."
+    );
+
+    // Scroll all the way to the bottom.
+    t.iframe.contentWindow.scrollTo(0, 20000);
+};
+
+t.open_iframe("/admin/index.cgi?admin_wiki", {w: 950});
+
+})(jQuery);

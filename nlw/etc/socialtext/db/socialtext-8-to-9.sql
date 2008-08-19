e07e0e909501e342b6051332b45e37f46e3903ba
Jeremy Stashewsky <jeremy.stashewsky@socialtext.com>
2008-08-19 03:11:08 +0000

    Migration for first/last names from person table.
    Moves first/last names set in the person table into the "User" table, but only
    wherever the "User" table entry is empty or just whitespace.
    
    Removes the first_name, last_name, name and email columns.
    
    middle_name was also removed in this iteration, so the 8-to-9 migration should
    still be valid.
    
    
    git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/trunk@9163 593ce9bc-2028-0410-bb98-c566b643a948

--- nlw/etc/socialtext/db/socialtext-8-to-9.sql
+++ nlw/etc/socialtext/db/socialtext-8-to-9.sql
@@ -1,9 +1,37 @@
 BEGIN;
 
--- Remove the middle_name field from the person table
+-- update the User table with profile-selected names
+
+UPDATE "User"
+SET first_name = person.first_name
+FROM person
+WHERE "User".first_name ~ '^\\s*'
+  AND "User".user_id = person.id
+  AND person.first_name IS NOT NULL;
+
+UPDATE "User"
+SET last_name = person.last_name
+FROM person
+WHERE "User".last_name ~ '^\\s*'
+  AND "User".user_id = person.id
+  AND person.last_name IS NOT NULL;
+
+-- Remove the name, first_name, middle_name, last_name and email fields from the person table
+
+ALTER TABLE person
+    DROP COLUMN name;
+
+ALTER TABLE person
+    DROP COLUMN first_name;
 
 ALTER TABLE person
     DROP COLUMN middle_name;
+
+ALTER TABLE person
+    DROP COLUMN last_name;
+
+ALTER TABLE person
+    DROP COLUMN email;
 
 UPDATE "System"
    SET value = 9

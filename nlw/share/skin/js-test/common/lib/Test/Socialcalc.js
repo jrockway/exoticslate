0ee1125a0a78e374c129ffa956cb5b146168e8ef
Ingy Dot Net <ingy.dot.net@socialtext.com>
2008-08-19 10:22:15 +0000

    Finish SocialCalc test refactoring.
    git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/trunk@9190 593ce9bc-2028-0410-bb98-c566b643a948

--- nlw/share/skin/js-test/common/lib/Test/Socialcalc.js
+++ nlw/share/skin/js-test/common/lib/Test/Socialcalc.js
@@ -1,9 +1,9 @@
-(function() {
-
-Test.Socialcalc = function() {
+// Class Test.Socialcalc
+(function(className) {
+var proto = (Test.Socialcalc = function() {
     this.init.apply(this, arguments);
-}
+    this.className = className;
+}).prototype = new Test.Base();
 
-var proto = Test.Socialcalc.prototype = new Test.Base();
 
-})();
+})('Test.Socialcalc');

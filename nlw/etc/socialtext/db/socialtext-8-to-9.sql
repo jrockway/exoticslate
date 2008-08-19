cb3cd7a69004a89eb9f5698bc1678e4177b80e68
Jeremy Stashewsky <jeremy.stashewsky@socialtext.com>
2008-08-19 17:42:43 +0000

    Update migration to meet stricter requirements.
    * lando++ and GabeW++ for the review.
    
    git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/trunk@9194 593ce9bc-2028-0410-bb98-c566b643a948

--- nlw/etc/socialtext/db/socialtext-8-to-9.sql
+++ nlw/etc/socialtext/db/socialtext-8-to-9.sql
@@ -3,18 +3,12 @@ BEGIN;
 -- update the User table with profile-selected names
 
 UPDATE "User"
-SET first_name = person.first_name
+SET first_name = person.first_name,
+    last_name = person.last_name
 FROM person
-WHERE "User".first_name ~ '^\\s*'
+WHERE "User".first_name ~ '^\\s*$' AND "User".last_name ~ '^\\s*$'
   AND "User".user_id = person.id
-  AND person.first_name IS NOT NULL;
-
-UPDATE "User"
-SET last_name = person.last_name
-FROM person
-WHERE "User".last_name ~ '^\\s*'
-  AND "User".user_id = person.id
-  AND person.last_name IS NOT NULL;
+  AND person.first_name IS NOT NULL AND person.last_name IS NOT NULL;
 
 -- Auto-vivify shouldn't set the username anymore; just a blank profile
 

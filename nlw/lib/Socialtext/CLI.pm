55a77a8c9a91762c8495ed3ee98d9af7c4d98895
Luke Closs <gravatar@5thplane.com>
2008-08-20 22:30:01 +0000

    print the user adding successes as they happen, to give feedback on the script
    git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/trunk@9265 593ce9bc-2028-0410-bb98-c566b643a948

--- nlw/lib/Socialtext/CLI.pm
+++ nlw/lib/Socialtext/CLI.pm
@@ -368,8 +368,13 @@ sub mass_add_users {
     eval {
         Socialtext::MassAdd->users(
             csv     => $csv,
-            pass_cb => sub { push @messages, $_[0] },
-            fail_cb => sub { push @messages, $_[0]; $has_errors++ },
+            pass_cb => sub { 
+                print $_[0], "\n";
+            },
+            fail_cb => sub { 
+                push @messages, $_[0]; 
+                $has_errors++;
+            },
         );
     };
     if ($@) {

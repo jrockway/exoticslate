9c0db946e97210a07f3bd14acd64a53a189b3359
Kevin Jones <kevin.jones@socialtext.com>
2008-08-26 14:32:48 +0000

    Add a failing test for even columns, and fix it
    git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/trunk@9374 593ce9bc-2028-0410-bb98-c566b643a948

---
+++ nlw/share/skin/js-test/s3/t/dashboard-evencols.t.js
@@ -0,0 +1,35 @@
+(function($) {
+
+var WIDTH_MIN = 100;
+var WIDTH_MAX = 1500;
+var WIDTH_INT = 10;
+
+var t = new Test.Visual();
+
+t.plan(1);
+
+t.beginAsync(step1);
+
+function step1() {
+    t.open_iframe("/", step2);
+}
+
+function step2() {
+    var always_even = true;
+    for (var width = WIDTH_MIN; width <= WIDTH_MAX; width += WIDTH_INT) {
+        $(t.iframe).width(width);
+        t.scrollTo(t.$('#contentLeft').offset().top, width);
+
+        always_even = ( t.$('#contentLeft').offset().top ==
+                        t.$('#columnMiddle').offset().top ) &&
+                      ( t.$('#columnMiddle').offset().top ==
+                        t.$('#contentRight').offset().top );
+        if (!always_even) break;
+    }
+
+    t.ok(always_even, always_even ? 'Columns are alway even' : 'Columns not even at ' + width);
+
+    t.endAsync();
+};
+
+})(jQuery);

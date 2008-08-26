865976e190118471a52328c35764589523458157
Ingy Dot Net <ingy.dot.net@socialtext.com>
2008-08-26 02:21:28 +0000

    Make this test login itself.
    git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/trunk@9370 593ce9bc-2028-0410-bb98-c566b643a948

--- nlw/share/skin/js-test/socialcalc/t/bz-972.t.js
+++ nlw/share/skin/js-test/socialcalc/t/bz-972.t.js
@@ -4,13 +4,18 @@ var t = tt = new Test.SocialCalc();
 
 t.plan(1);
 
-t.beginAsync(step1);
+t.beginAsync(login);
+
+function login() {
+    t.login({callback: step1});
+}
 
 function step1() {
     t.open_iframe_with_socialcalc("/admin/index.cgi?action=display;page_type=spreadsheet;page_name=banana#edit", step2);
 }
 
 function step2() {
+    t.iframe.contentWindow.onbeforeunload = null;
     var val = t.callEventHandler("#st-color-button-link", "click");
     t.is(val, false, "Event handler returns false");
 

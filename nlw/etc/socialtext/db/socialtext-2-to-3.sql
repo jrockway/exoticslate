dinda/socialtext

commit 55ec80c5a7c1d060ccdbefee3bb9da85d8b6291f

Don't load extra plugin lib paths, now that they are in a normal place
git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/branches/iteration-2008-08-22@9416 593ce9bc-2028-0410-bb98-c566b643a948

nlw/lib/Socialtext/Pluggable/Adapter.pm
@@ -15,11 +15,6 @@ use Module::Pluggable search_path => ['Socialtext::Pluggable::Plugin'],
                       search_dirs => \@libs;
 use Socialtext::Pluggable::WaflPhrase;
 
-BEGIN {
-    our $code_base = Socialtext::AppConfig->code_base;
-    push @INC, glob("$code_base/plugin/*/lib");
-}
-
 sub AUTOLOAD {
     my ($self,$rest_handler,$args) = @_;
     my $type = ref($self)

jrockway/exoticslate

commit 4b71d0a1a22700dfbfc590d31f5be4a2a301bdb5

smerge iteration-2008-08-22 to trunk  r16232@topaz (orig r9416):  lukec | 2008-08-27 16:32:49 -0700  Don't load extra plugin lib paths, now that they are in a normal place  r16241@topaz (orig r9423):  audreyt | 2008-08-27 21:01:47 -0700  * Fix the damage caused by r552 to 08-22:
     r552 | lukec | 2008-08-27 16:30:48 -0700 (Wed, 27 Aug 2008) | 1 line
     Plugin lib files should live with the rest of the perl code.
 
 * Full context below:
 
 ----- "Luke Closs" <luke.closs@socialtext.com> wrote:
 > 1) Plugin code is installed in a non-standard location
 > #1 is kind of a stupid design thing that stash and I just fixed.  The
 > migration was doing 'use Socialtext::People::Profile', but that module
 > lived off in /usr/share/nlw/plugin/people/lib, which perl doesn't know
 > about by default.  Apache was setting up a special path for those  
 > plugins, so it just worked there.  This is now fixed so that plugins
 > install their perl code in /usr/share/perl5, like everything else.
 
 Luke, this fix broke www2.  The Socialtext/Pluggable/Plugin.pm had this line:
 
     $name =~ s{^$code_base/plugin/([^/]+).*$}{$1} or $name = 'gadgets';
 
 which assumed the pre-this-fix location.
 
   I've fixed it to www2 and will commit a proper fix shortly to trunk and 0822.
 
 
 r16242@topaz (orig r9424):  audreyt | 2008-08-27 23:44:39 -0700
 * Reverse the damage to dev-env caused by r9416.
 
 ------------------------------------------------------------------------
 r9416 | lukec | 2008-08-27 16:32:49 -0700 (Wed, 27 Aug 2008) | 1 line
 Don't load extra plugin lib paths, now that they are in a normal place
 ------------------------------------------------------------------------
 
 
 r16287@topaz (orig r9460):  lukec | 2008-08-29 11:39:34 -0700
 update-branch iteration-2008-08-22 from iteration-2008-08-08
 r16292@topaz (orig r9462):  lukec | 2008-08-29 12:26:46 -0700
 missing on delete cascade contstraint
 r16322@topaz (orig r9479):  lukec | 2008-08-30 02:49:33 -0700

nlw/etc/socialtext/db/socialtext-2-to-3.sql
@@ -14,6 +14,7 @@ $$ LANGUAGE 'plpgsql' VOLATILE;
 
 -- Remove a storage table created by old versions of ST::Storage::PSQL
 SELECT execute_if_table_exists('storage', 'drop table storage');
+DROP FUNCTION execute_if_table_exists(text,text);
 
 CREATE TABLE "storage" (
     user_id bigint NOT NULL,

nlw/etc/socialtext/db/socialtext-6-to-7.sql
@@ -69,10 +69,28 @@ CREATE INDEX ix_event_tag
 	    ON event (tag_name, at)
             WHERE (event_class = 'page' OR event_class = 'person');
 
+ALTER TABLE ONLY event
+    ADD CONSTRAINT event_actor_id_fk
+            FOREIGN KEY (actor_id)
+            REFERENCES "UserId"(system_unique_id) ON DELETE CASCADE;
+
+ALTER TABLE ONLY event
+    ADD CONSTRAINT event_page_fk
+            FOREIGN KEY (page_workspace_id, page_id)
+            REFERENCES page(workspace_id, page_id) ON DELETE CASCADE;
+
+ALTER TABLE ONLY event
+    ADD CONSTRAINT event_person_id_fk
+            FOREIGN KEY (person_id)
+            REFERENCES "UserId"(system_unique_id) ON DELETE CASCADE;
+
 ALTER TABLE tag_people__person_tags 
     DROP CONSTRAINT tag_people_fk;
 
 ALTER TABLE tag RENAME TO person_tag;
+
+CREATE UNIQUE INDEX person_tag__name
+        ON person_tag (name);
 
 ALTER TABLE ONLY tag_people__person_tags
     ADD CONSTRAINT tag_people_fk

nlw/etc/socialtext/db/socialtext-7-to-8.sql
@@ -9,6 +9,36 @@ ALTER TABLE person
 ALTER TABLE person
     ADD COLUMN small_photo_image bytea;
 
+-- Table was renamed, so we should re-name the pkey index to be consistent
+-- Dropping the constraint cascades, so we need to re-create tag_people_fk
+-- too.
+ALTER TABLE ONLY person_tag DROP CONSTRAINT tag_pkey CASCADE;
+ALTER TABLE ONLY person_tag ADD CONSTRAINT person_tag_pkey PRIMARY KEY (id);
+ALTER TABLE ONLY tag_people__person_tags
+    ADD CONSTRAINT tag_people_fk
+            FOREIGN KEY (tag_id)
+            REFERENCES person_tag(id) ON DELETE CASCADE;
+
+
+-- Remove the execute_(unless|if)_table_exists - these shouldn't be left
+-- in the core schema.  Make sure they exist before we delete them.
+
+CREATE OR REPLACE FUNCTION execute_if_table_exists (table_name TEXT, sql TEXT) RETURNS BOOLEAN AS $$
+BEGIN
+    RETURN(FALSE);
+END
+$$ LANGUAGE 'plpgsql' VOLATILE;
+
+CREATE OR REPLACE FUNCTION execute_unless_table_exists (table_name TEXT, sql TEXT) RETURNS BOOLEAN AS $$
+BEGIN
+    RETURN(FALSE);
+END
+$$ LANGUAGE 'plpgsql' VOLATILE;
+
+DROP FUNCTION execute_if_table_exists(text,text);
+DROP FUNCTION execute_unless_table_exists(text,text);
+
+
 UPDATE "System"
    SET value = 8
  WHERE field = 'socialtext-schema-version';

nlw/etc/socialtext/db/socialtext-schema.sql
@@ -597,7 +597,7 @@ ALTER TABLE ONLY person_watched_people__person
 ALTER TABLE ONLY tag_people__person_tags
     ADD CONSTRAINT tag_people_fk
             FOREIGN KEY (tag_id)
-            REFERENCES person_tag(id);
+            REFERENCES person_tag(id) ON DELETE CASCADE;
 
 ALTER TABLE ONLY "UserWorkspaceRole"
     ADD CONSTRAINT userworkspacerole___role___role_id___role_id___n___1___1___0

nlw/lib/Socialtext.pm
@@ -15,7 +15,7 @@ use Socialtext::Authz;
 use Socialtext::Permission 'ST_READ_PERM';
 use Socialtext::Validate qw( validate SCALAR_TYPE USER_TYPE WORKSPACE_TYPE );
 
-our $VERSION = '2.22.2.2';
+our $VERSION = '2.22.2.3';
 
 const product_version => $VERSION;
 field using_debug => 0;

nlw/lib/Socialtext/Pluggable/Adapter.pm
@@ -16,6 +16,7 @@ use Module::Pluggable search_path => ['Socialtext::Pluggable::Plugin'],
 use Socialtext::Pluggable::WaflPhrase;
 
 BEGIN {
+    # This is still needed for dev-env -- Do Not Delete!
     our $code_base = Socialtext::AppConfig->code_base;
     push @INC, glob("$code_base/plugin/*/lib");
 }

nlw/share/migrations/27-person-photos-as-blobs/migration
@@ -1,11 +1,10 @@
 #!/usr/bin/env perl
 # @COPYRIGHT@
-
 use strict;
 use warnings;
-
 use File::Temp qw/tmpnam/;
 use LWP::UserAgent;
+use Socialtext::User; # loaded here to avoid a circular dependency
 use Socialtext::Migration::Utils qw/ensure_socialtext_schema/;
 use Socialtext::People::Profile;
 use Socialtext::SQL qw/sql_begin_work sql_execute sql_commit/;
@@ -36,7 +35,7 @@ for my $row ( @{ $result->fetchall_arrayref } ) {
 
     # if this blows up, let's skip it.
     eval { $profile->save_profile_image( $blob ) };
-    warn "WARNING: $@\n" if ( $@ );
+    warn "Error saving profile image: $@\n" if $@;
 }
 sql_commit();
 

nlw/share/migrations/27-person-photos-as-blobs/post-check
@@ -1,9 +1,7 @@
 #!/usr/bin/env perl
 # @COPYRIGHT@
-
 use strict;
 use warnings;
-
 use Socialtext::SQL qw/sql_singlevalue/;
 
 # Basically stated, this query reads: what's the datatype of the person.photo

nlw/share/migrations/27-person-photos-as-blobs/pre-check
@@ -1,22 +1,27 @@
 #!/usr/bin/env perl
 # @COPYRIGHT@
-
 use strict;
 use warnings;
-
 use Socialtext::SQL qw/sql_singlevalue/;
+
+# check that ST People is installed
+eval "require Socialtext::Plugin::People";
+if ($@) {
+    print "Socialtext People is not installed, skipping.\n";
+    exit 1;
+}
 
 # Basically stated, this query reads: what's the datatype of the person.photo
 # field?
 my $result = sql_singlevalue( <<EOSQL);
-SELECT count( a.attname )
+SELECT count(a.attname)
   FROM pg_attribute a 
   JOIN pg_stat_user_tables ut ON ut.relid = a.attrelid
  WHERE a.attname = 'photo_image'
    AND ut.relname = 'person';
 EOSQL
 
-if ( $result == 1  ) {
+if ($result == 1) {
     print "It looks like your 'person' table is up to date, skipping.\n";
     exit 1;
 }
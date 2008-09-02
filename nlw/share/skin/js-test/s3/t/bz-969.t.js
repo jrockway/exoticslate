a15910c184c96fd16ae8dbd6633e68cc1a6c0b3b
Audrey Tang <audrey.tang@socialtext.com>
2008-09-02 23:30:57 +0000

    * Add IE7 test based on shahed's screenshot to {bz: 969}'s js-test.
    git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/trunk@9526 593ce9bc-2028-0410-bb98-c566b643a948

--- nlw/share/skin/js-test/s3/t/bz-969.t.js
+++ nlw/share/skin/js-test/s3/t/bz-969.t.js
@@ -2,7 +2,7 @@
 
 var t = new Test.Visual();
 
-t.plan(1);
+t.plan(2);
 
 t.beginAsync(function(){
     t.open_iframe("/admin/index.cgi?action=weblog_display&category=TEST" + Math.random(), function(){
@@ -13,6 +13,13 @@ t.beginAsync(function(){
         "In weblog view, page navs shouldn't overlap on each other"
     );
     
+
+    t.elements_do_not_overlap(
+        t.$('#st-weblog-newpost-button'),
+        t.$('#contentRight'),
+        "Also page navs shouldn't overlap with the right-side content pane"
+        + " (bugs.socialtext.net:555/attachment.cgi?id=87)"
+    );
     t.endAsync();
 })});
 

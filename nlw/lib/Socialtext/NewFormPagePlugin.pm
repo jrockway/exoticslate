jrockway/exoticslate

commit 25a03b8217fcc0c8572c69ea05c8ffda87104922

Refactor Socialtext::Skin to represent a skin rather than an API to all skins. This will make it easier to disable the homepage based on a base skin.
git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/trunk@9868 593ce9bc-2028-0410-bb98-c566b643a948

nlw/lib/Socialtext/NewFormPagePlugin.pm
@@ -25,8 +25,9 @@ sub register {
 
 sub template_path {
     my $self = shift;
+    my $skin = Socialtext::Skin(name => 's2');
     return join '/',
-        $self->hub->skin->skin_path('s2'),
+        $skin->skin_path,
         'template/new_form_page',
         @_;
 }

nlw/lib/Socialtext/Handler/Authen.pm
@@ -51,7 +51,9 @@ sub handler ($$) {
             username_label => $self->username_label,
             redirect_to    => $self->{args}{redirect_to},
             static_path    => Socialtext::Helpers::static_path(),
-            skin_uri       => sub { Socialtext::Skin->skin_uri(shift) },
+            skin_uri       => sub {
+                Socialtext::Skin->new(name => shift)->skin_uri
+            },
             st_version     => $Socialtext::VERSION,
             support_address => Socialtext::AppConfig->support_address,
             %$saved_args,

nlw/lib/Socialtext/Helpers.pm
@@ -200,9 +200,7 @@ sub global_template_vars {
 
     my $search_box = $renderer->render(
         template => \$snippet,
-        paths => [
-            $self->hub->skin->skin_path('s2') . '/template'
-        ],
+        paths => $self->hub->skin->template_paths,
         vars => {
             current_workspace => $self->hub->current_workspace,
             show_search_set   => $show_search_set,
@@ -292,7 +290,9 @@ sub _get_wiki_info {
         email_address => $wiki->email_in_address,
         static_path   => $self->static_path,
         skin_uri      => sub { 
-            $self->hub->skin->skin_uri( shift || $skin )
+            my $skin_name  = shift;
+            my $skin = Socialtext::Skin->new(name => $skin_name);
+            return $skin->skin_uri();
         },
         comment_form_window_height => $wiki->comment_form_window_height,
         system_status              => $self->hub->main ?
@@ -300,12 +300,6 @@ sub _get_wiki_info {
         comment_by_email           => $wiki->comment_by_email,
         email_in_address           => $wiki->email_in_address,
     };
-}
-
-sub skin_uri {
-    my $skin = Socialtext::Skin->new();
-    my $skin_name  = shift;
-    return $skin->skin_uri($skin_name);
 }
 
 1;

nlw/lib/Socialtext/InitHandler.pm
@@ -33,11 +33,10 @@ sub _regen_combined_js {
     my ($ws_name) = $uri =~ m{^/([^/]+)/index\.cgi$};
     my $workspace = $ws_name ? Socialtext::Workspace->new(name=>$ws_name)
                              : Socialtext::NoWorkspace->new;
-    my $skin_name = $workspace->skin_name;
 
-    my $skin = Socialtext::Skin->new;
+    my $skin = Socialtext::Skin->new(name => $workspace->skin_name);
 
-    for my $dir ($skin->make_dirs($skin_name)) {
+    for my $dir ($skin->make_dirs) {
         local $CWD = $dir;
 
         my $semaphore = "$dir/build-semaphore";

nlw/lib/Socialtext/Skin.pm
@@ -8,6 +8,7 @@ use base 'Socialtext::Plugin';
 use File::Basename qw(dirname);
 use Socialtext::URI;
 use Socialtext::AppConfig;
+use Socialtext::Workspace;
 use Socialtext;
 use File::Spec;
 use YAML;
@@ -23,89 +24,100 @@ my %css_files = (
 
 sub class_id { 'skin' }
 
-# Returns an array of full paths to the preloaded templates
+# Returns an array of full paths to the preloaded templates, unless the skin
+# is a symlink
 sub PreloadTemplateDirs {
     my $class = shift;
-    return glob($class->_path('skin', '*', 'template'));
+    return grep { !-l $_ } glob($class->_path('skin', '*', 'template'));
+}
+
+sub new {
+    my ($class, %args) = @_;
+    my $self = $class->SUPER::new(%args);
+    $self->{_no_workspace} = Socialtext::NoWorkspace->new;
+    $self->{_skin_name} = $args{name};
+    return -d $self->skin_path ? $self : undef;
+}
+
+sub workspace {
+    my ($self) = @_;
+    return $self->hub
+        ? $self->hub->current_workspace
+        : $self->{_no_workspace};
+}
+
+sub parent {
+    my $self = shift;
+    my $parent_skin = $self->skin_info->{parent};
+    return Socialtext::Skin->new(name => $parent_skin) if $parent_skin;
 }
 
 sub template_paths {
     my ($self,$skin) = @_;
-    my $info = $self->skin_info($skin);
-    return [
-        grep { -d $_ }
-        map { $self->skin_path($_) . '/template' }
-        reverse $self->inheritence($skin)
-    ];
+    my $info = $self->skin_info;
+
+    my $dirs = $self->parent ? $self->parent->template_paths : [];
+    push @$dirs, grep { -d $_ } $self->skin_path('template');
+    return $dirs;
+}
+
+sub base_skin {
+    my ($self, $skin) = @_;
+    my $base = $self;
+    1 while $base = $base->parent;
+    return $base;
 }
 
 sub skin_info {
-    my ($self, $skin) = @_;
-    $skin ||= $self->current_skin;
-    return $self->{_skin_info}{$skin} if $self->{_skin_info}{$skin};
-    my $skin_path = $skin =~ m{^uploaded-skin/}
-        ? $self->_path($skin)
-        : $self->skin_path($skin);
+    my $self = shift;
+    my $skin = $self->skin_name;
+
+    return $self->{_skin_info} if $self->{_skin_info};
+
+    my $skin_path = $self->skin_path;
+
     my $info_path = File::Spec->catfile( $skin_path, 'info.yaml' );
-    $self->{_skin_info} = -f $info_path ? YAML::LoadFile($info_path) : {};
-    $self->{_skin_info}{parent} ||= $DEFAULT_PARENT;
-    $self->{_skin_info}{skin_name} = $skin;
-    $self->{_skin_info}{skin_path} = $skin_path;
-    unless (defined $self->{_skin_info}{cascade_css}) {
-        $self->{_skin_info}{cascade_css} =
-            $self->hub->current_workspace->cascade_css;
+    my $info = -f $info_path ? YAML::LoadFile($info_path) : {};
+    unless (exists $info->{parent}) {
+        $info->{parent} = $DEFAULT_PARENT if $DEFAULT_PARENT ne $skin;
     }
-    return $self->{_skin_info};
+    $info->{skin_name} = $skin;
+    $info->{skin_path} = $skin_path;
+    $info->{cascade_css} = defined $info->{cascade_css}
+        ? $info->{cascade_css}
+        : $self->workspace->cascade_css;
+
+    return $self->{_skin_info} = $info;
 }
 
-sub inheritence {
-    my ($self,$skin) = @_;
-    $skin ||= $self->current_skin;
-
-    return @{$self->{_inheritence}{$skin}} if $self->{_inheritence}{$skin};
-
-    my %done;
-    my @inherit;
-    while ($skin and not $done{$skin}) {
-        $done{$skin} = 1; # protect against infinit loops
-        my $info = $self->skin_info($skin);
-        push @inherit, $skin;
-        $skin = $info->{parent};
-    }
-    $self->{_inheritence}{$skin} = \@inherit;
-    return @inherit;
+sub no_common {
+    my $self = shift;
+    return $self->skin_info->{no_common} ||
+           ($self->parent && $self->parent->no_common);
 }
 
 sub css_info {
     my $self = shift;
     my $skin_info = $self->skin_info;
 
-    my %files;
+    my $css_info = ($self->parent && $skin_info->{cascade_css})
+        ? $self->parent->css_info
+        : {};
 
-    my $add_common = 1;
-
-    for my $skin ($self->inheritence) {
-        my $info = $self->skin_info($skin);
-
-        my $skin_path = $self->skin_path($skin);
-        my $skin_uri = $self->skin_uri($skin);
-
-        while (my ($sec,$files) = each %css_files) {
-            unshift @{$files{$sec}}, map  { "$skin_uri/css/$_" }
-                                     grep { -f "$skin_path/css/$_" }
-                                     @$files;
-        }
-
-        $add_common = 0 if $info->{no_common};
-        last unless $info->{cascade_css};
+    while (my ($sec,$files) = each %css_files) {
+        push @{$css_info->{$sec}}, map  { $self->skin_uri('css',$_) }
+                                   grep { -f $self->skin_path('css',$_) }
+                                   @$files;
     }
 
     # Common CSS
-    if ($add_common) {
-        push @{$files{common}}, $self->_uri('skin/common/css/common.css');
+    unless ($self->no_common) {
+        $css_info->{common} ||= [
+             $self->_uri('skin/common/css/common.css')
+        ];
     }
 
-    return \%files;
+    return $css_info;
 }
 
 sub css_files {
@@ -125,40 +137,30 @@ sub css_files {
     return @files;
 }
 
-sub current_skin {
-    my $self = shift;
-    if ($self->hub->current_workspace->uploaded_skin) {
-        my $ws = $self->hub->current_workspace->name;
-        return "uploaded-skin/$ws";
-    }
-    else {
-        return $self->hub->current_workspace->skin_name;
-    }
-}
-
 sub skin_upload_path {
     my $self = shift;
-    my $ws = $self->hub->current_workspace->name;
-    return $self->_path("uploaded-skin/$ws");
+    my $workspace_name = $self->workspace->name;
+    return $self->_path("uploaded-skin/$workspace_name");
 }
 
 sub skin_path {
     my $self = shift;
-    my $skin = shift || $self->current_skin;
+    my $skin = $self->skin_name;
     if ($skin =~ m{^(?:skin|uploaded-skin)/}) {
-        return $self->_path($skin);
+        return $self->_path($skin, @_);
     }
-    return $self->_path('skin', $skin);
+    return $self->_path('skin', $skin, @_);
 }
 
 sub skin_uri {
     my $self = shift;
+    my $skin = $self->skin_name;
 
-    my $skin = shift || $self->current_skin;
+    my $skin_uri;
     if ($skin =~ m{^(?:skin|uploaded-skin)/}) {
-        return $self->_uri($skin);
+        return $self->_uri($skin, @_);
     }
-    return $self->_uri('skin', $skin);
+    return $self->_uri('skin', $skin, @_);
 }
 
 sub customjs {
@@ -166,15 +168,15 @@ sub customjs {
     my ($uri, $name);
 
     # Ignore customjs_name and customjs_uri if we're using an uploaded skin
-    unless ($self->hub->current_workspace->uploaded_skin) {
-        $uri = $self->hub->current_workspace->customjs_uri;
+    unless ($self->workspace->uploaded_skin) {
+        $uri = $self->workspace->customjs_uri;
         $name = $self->customjs_name;
     }
 
     unless ($uri) {
         my $path = $self->skin_path($name);
         if (-f "$path/javascript/custom.js") {
-            $uri = $self->skin_uri($name) . '/javascript/custom.js';
+            $uri = $self->skin_uri($name, 'javascript/custom.js');
         }
     }
 
@@ -184,8 +186,10 @@ sub customjs {
 sub skin_name {
     my $self = shift;
     require Apache::Cookie;
-    return $self->{skin_name}
-        if defined $self->{skin_name};
+    return $self->{_skin_name} if defined $self->{_skin_name};
+
+    my $workspace_name = $self->workspace->name;
+
     my $skin_name;
     my $self_uri = Socialtext::URI::uri();
     if ( Apache::Cookie->can('fetch') ) {
@@ -197,21 +201,20 @@ sub skin_name {
             }
         }
     }
-    return $skin_name ||
-        $self->hub->current_workspace->skin_name;
+    return $skin_name || $self->workspace->uploaded_skin
+            ? "uploaded-skin/$workspace_name"
+            : $self->workspace->skin_name;
 }
 
 sub cascade_css {
     my $self = shift;
     my $cascade = $self->skin_info->{cascade_css};
-    return defined $cascade ? $cascade : 
-                              $self->hub->current_workspace->cascade_css;
+    return defined $cascade ? $cascade : $self->workspace->cascade_css;
 }
 
 sub customjs_name {
     my $self = shift;
-    return $self->skin_info->{customjs_name} ||
-        $self->hub->current_workspace->customjs_name;
+    return $self->skin_info->{customjs_name} || $self->workspace->customjs_name;
 }
 
 sub header_logo_image_uri {
@@ -236,12 +239,12 @@ sub header_logo_image_uri {
 }
 
 sub make_dirs {
-    my ($self, $skin) = @_;
-    return 
-        map { dirname($_) }
-        grep { -f $_ }
-        map { $self->_path("skin/$_/javascript/Makefile") }
-        $self->inheritence($skin);
+    my $self = shift;
+    my @dirs;
+    push @dirs, $self->parent->make_dirs if $self->parent;
+    my $makefile = $self->skin_path("javascript/Makefile");
+    push @dirs, dirname($makefile) if -f $makefile;
+    return @dirs;
 }
 
 sub _path {

nlw/lib/Socialtext/Workspace.pm
@@ -512,10 +512,10 @@ sub _validate_and_clean_data {
         push @errors, loc('Incoming email placement must be one of top, bottom, or replace.');
     }
 
-    if ( $p->{skin_name}
-         && ! 1 # Socialtext::Skin->new( name => $p->{skin_name} )
-       ) {
-        push @errors, loc("The skin you specified,[_1], does not exist.", $p->{skin_name});
+    if ( $p->{skin_name} && ! 1) {
+        push @errors, loc(
+            "The skin you specified,[_1], does not exist.", $p->{skin_name}
+        );
     }
 
     if ( $is_create and not $p->{account_id} ) {

nlw/share/skin/s3/template/view/listview
@@ -8,11 +8,11 @@
     </div><!-- controlsLeft END -->
     <div id="controlsRight">
         [% IF feeds.rss.page %]
-            <a style="float: left; margin-top: 12px" href="[% feeds.rss.page.url %]"><img border="0" src="[% wiki.skin_uri('s3/images/rss.png') %]"/></a>
+            <a style="float: left; margin-top: 12px" href="[% feeds.rss.page.url %]"><img border="0" src="[% wiki.skin_uri('s3') %]/images/rss.png"/></a>
         [% END %]
         [% IF hub.current_workspace.enable_unplugged %]
             [% IF unplug_uri %]
-                <a style="float: left; margin-top: 12px; margin-left: 5px;" title="[% unplug_phrase %]" href="[% unplug_uri %]"><img border="0" src="[% wiki.skin_uri('s3/images/plug.png') %]"/></a>
+                <a style="float: left; margin-top: 12px; margin-left: 5px;" title="[% unplug_phrase %]" href="[% unplug_uri %]"><img border="0" src="[% wiki.skin_uri('s3') %]/images/plug.png"/></a>
             [% END %]
         [% END %]
       <ul class="level1">

nlw/share/skin/s3/template/view/page/display
@@ -258,12 +258,12 @@
 
 [% IF enable_unplugged %]
     <a title="[% loc('Click this button to save this page to your computer for offline use.') %]" href="?action=unplug;page_name=[% page.id %]" style="float: right; margin-left: 5px">
-      <img class="st-page-details-feed-icon" alt="[% loc('Click this button to save this page to your computer for offline use.') %]" src="[% wiki.skin_uri('s3/images/plug.png') %]" />
+      <img class="st-page-details-feed-icon" alt="[% loc('Click this button to save this page to your computer for offline use.') %]" src="[% wiki.skin_uri('s3') %]/images/plug.png" />
      </a>
 [% END %]
 [% IF feeds.rss %]
     <a href="[% feeds.rss.page.url %]" style="float: right">
-        <img class="st-page-details-feed-icon" src="[% wiki.skin_uri('s3/images/rss.png') %]" />
+        <img class="st-page-details-feed-icon" src="[% wiki.skin_uri('s3') %]/images/rss.png" />
     </a>
 [% END %]
 

commit b00f505b70788319af9cc26bbf2394c0f657413b

Fix accidental breakage from r9868.
git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/trunk@9898 593ce9bc-2028-0410-bb98-c566b643a948

nlw/lib/Socialtext/NewFormPagePlugin.pm
@@ -8,6 +8,7 @@ use base 'Socialtext::Plugin';
 use Class::Field qw( const );
 use Socialtext::AppConfig;
 use Socialtext::BrowserDetect ();
+use Socialtext::Skin;
 use Socialtext::l10n qw(loc);
 
 sub class_id { 'new_form_page' }
@@ -25,7 +26,7 @@ sub register {
 
 sub template_path {
     my $self = shift;
-    my $skin = Socialtext::Skin(name => 's2');
+    my $skin = Socialtext::Skin->new(name => 's2');
     return join '/',
         $skin->skin_path,
         'template/new_form_page',
799ab6c170ae35f2df2b20ce468a205ee610c386
Jeremy Stashewsky <jeremy.stashewsky@socialtext.com>
2008-09-13 02:25:00 +0000

    Fix JOINs for user names (luke++)
    git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/trunk@9929 593ce9bc-2028-0410-bb98-c566b643a948

--- nlw/etc/socialtext/db/dev.sql
+++ nlw/etc/socialtext/db/dev.sql
@@ -2,7 +2,6 @@
 -- These are useful views and tricks for debugging Socialtext Schemas
 --
 
--- DROP VIEW xpage;
 CREATE OR REPLACE VIEW xpage AS 
     SELECT "Workspace".name AS workspace_name, 
            page.*,
@@ -10,30 +9,30 @@ CREATE OR REPLACE VIEW xpage AS
            editor.username AS editor_username 
         FROM page 
             JOIN "Workspace" ON (page.workspace_id = "Workspace".workspace_id) 
-            JOIN "User" editor  ON (page.last_editor_id = editor.user_id) 
-            JOIN "User" creator ON (page.creator_id    = creator.user_id);
+            JOIN "UserId" editorid  ON (page.last_editor_id = editorid.system_unique_id) 
+            JOIN "UserId" creatorid  ON (page.creator_id = creatorid.system_unique_id) 
+            LEFT JOIN "User" editor  ON (editorid.driver_unique_id = editor.user_id) 
+            LEFT JOIN "User" creator ON (creatorid.driver_unique_id = creator.user_id);
 
--- DROP VIEW xpage_tag;
 CREATE OR REPLACE VIEW xpage_tag AS 
     SELECT "Workspace".name AS workspace_name, page_tag.*
         FROM page_tag
             JOIN "Workspace" ON (page_tag.workspace_id = "Workspace".workspace_id);
 
--- DROP VIEW xworkspace;
 CREATE OR REPLACE VIEW xworkspace AS 
     SELECT "Workspace".*, "Account".name AS account_name
         FROM "Workspace" 
             JOIN "Account" ON ("Workspace".account_id = "Account".account_id);
 
--- DROP VIEW xuwr;
 CREATE OR REPLACE VIEW xuwr AS 
     SELECT "User".username, 
            "Workspace".name AS workspace_name,
            "Role".name AS role_name
-        FROM "UserWorkspaceRole" 
-            JOIN "User" ON ("UserWorkspaceRole".user_id = "User".user_id)
-            JOIN "Workspace" ON ("UserWorkspaceRole".workspace_id = "Workspace".workspace_id)
-            JOIN "Role" ON ("UserWorkspaceRole".role_id = "Role".role_id);
+        FROM "UserWorkspaceRole" uwr
+            JOIN "Workspace" ON (uwr.workspace_id = "Workspace".workspace_id)
+            JOIN "Role" ON (uwr.role_id = "Role".role_id)
+            JOIN "UserId" ON (uwr.user_id = "UserId".system_unique_id)
+            LEFT JOIN "User" ON ("UserId".driver_unique_id = "User".user_id);
 
 CREATE OR REPLACE VIEW xevent AS
     SELECT e.at AS at, 
@@ -46,10 +45,10 @@ CREATE OR REPLACE VIEW xevent AS
            e.tag_name AS tag_name, 
            e.context AS context
     FROM event e 
-         LEFT JOIN "User" actor 
-            ON (e.actor_id = actor.user_id) 
-         LEFT JOIN "User" person 
-            ON (e.person_id = person.user_id)
+         LEFT JOIN "UserId" actorid ON (e.actor_id = actorid.system_unique_id)
+         LEFT JOIN "User" actor ON (actorid.driver_unique_id = actor.user_id) 
+         LEFT JOIN "UserId" personid ON (e.person_id = personid.system_unique_id)
+         LEFT JOIN "User" person ON (personid.driver_unique_id = person.user_id) 
          LEFT JOIN page p 
             ON (e.page_workspace_id = p.workspace_id AND e.page_id = p.page_id) 
          LEFT JOIN "Workspace" w 

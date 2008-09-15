917f72af7a70c22008e4f5c8801f57840dbc2219
Brandon Noard <brandon.noard@socialtext.com>
2008-09-15 18:46:54 +0000

    Add a AccountUsers ReST handler so we business admins can use the ReST API to add users to a primary account.
    git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/trunk@9967 593ce9bc-2028-0410-bb98-c566b643a948

---
+++ nlw/lib/Socialtext/Rest/AccountUsers.pm
@@ -0,0 +1,72 @@
+package Socialtext::Rest::AccountUsers;
+# @COPYRIGHT@
+
+use strict;
+use warnings;
+use base 'Socialtext::Rest::Collection';
+use Socialtext::Account;
+use Socialtext::JSON qw/decode_json/;
+use Socialtext::HTTP ':codes';
+use Socialtext::String;
+use Socialtext::User;
+
+sub allowed_methods { 'POST' }
+sub collection_name { 'Account Users' }
+
+sub POST_json {
+    my $self = shift;
+    my $rest = shift;
+    my $data = decode_json( $rest->getContent() );
+
+    unless ( $self->_user_is_business_admin_p ) {
+        $rest->header(
+            -status => HTTP_401_Unauthorized,
+        );
+        return '';
+    }
+
+    unless ( defined $data->{email_address} ) {
+        $rest->header(
+            -status => HTTP_400_Bad_Request,
+        );
+        return '';
+    }
+
+    my $account = Socialtext::Account->new( 
+        name => Socialtext::String::uri_escape( $self->acct ),
+    );
+
+    unless ( defined $account ) {
+        $rest->header(
+            -status => HTTP_400_Bad_Request,
+        );
+        return '';
+    }
+
+    my $user = Socialtext::User->new(
+        email_address => $data->{email_address},
+    );
+
+    unless ( defined $user ) {
+        $rest->header(
+            -status => HTTP_400_Bad_Request,
+        );
+        return '';
+    }
+
+    eval{ $user->primary_account($account->account_id); };
+
+    if ( $@ ) {
+        $rest->header(
+            -status => HTTP_400_Bad_Request,
+        );
+        return '';
+    }
+
+    $rest->header(
+        -status => HTTP_200_OK,
+    );
+    return '';
+}
+
+1;

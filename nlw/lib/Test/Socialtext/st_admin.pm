9a873b27cc10cef96b2df654fce72cd9625f61d9
Jeremy Stashewsky <jeremy.stashewsky@socialtext.com>
2008-09-15 18:44:07 +0000

    Make new st_admin helper class that allows you to run CLI commands a *lot*  quicker than shelling-out.
    git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/trunk@9966 593ce9bc-2028-0410-bb98-c566b643a948

---
+++ nlw/lib/Test/Socialtext/st_admin.pm
@@ -0,0 +1,31 @@
+#!perl
+# @COPYRIGHT@
+use warnings;
+use strict;
+
+use Test::More;
+use Socialtext::CLI;
+
+use base qw(Exporter);
+our @EXPORT = qw(&st_admin);
+
+my $status;
+{
+    no warnings qw(once redefine);
+    *Socialtext::CLI::_exit = sub { $status = shift };
+}
+
+# fakes runing st-admin on the command line.
+sub st_admin {
+    my $args = shift;
+    my @args = split(/\s+/,$args);
+    open my $fh, '>>t/tmp/cli.log';
+    {
+        local *STDERR = $fh;
+        local *STDOUT = $fh;
+        Socialtext::CLI->new(argv => [@args])->run();
+    }
+    return ok !$status, "st_admin $args";
+}
+
+END { unlink 't/tmp/cli.log' }

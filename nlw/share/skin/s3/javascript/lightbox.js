f09f38de7f1b860907564850b37857c47356683d
Kang-min Liu <kang-min.liu@socialtext.com>
2008-09-17 23:44:11 +0000

    Fixes {bz: 1293}.
    Althought it appears that jQuery does not select the iframe html element,
    but evidently, doing $("html").css("overflow", "hidden") also reflow
    the HTML in wysiwyg editor iframe.
    
    Prevents the html/iframe reflow by detecting if it's required to hide
    window scrollbar. When it is edit mode, usually we already adjust the
    editor height such that scrollbar is already gone.
    
    This solution is better then detecting if it's editor mode because
    that would make lightbox code depend on wikiwyg code.
    
    
    git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/trunk@10099 593ce9bc-2028-0410-bb98-c566b643a948

--- nlw/share/skin/s3/javascript/lightbox.js
+++ nlw/share/skin/s3/javascript/lightbox.js
@@ -74,8 +74,11 @@
 
         opts._originalHTMLOverflow = $('html').css('overflow') || 'visible';
         opts._originalBodyOverflow = $('body').css('overflow') || 'visible';
-        $('html,body').css('overflow', 'hidden');
-        $('html,body').attr('scrollTop', pageScroll.top);
+
+        if ($("body").height() > $(window).height()) {
+            $('html,body').css('overflow', 'hidden');
+            $('html,body').attr('scrollTop', pageScroll.top);
+        }
 
         if (opts.close)
             $(opts.close).click(function () { $.hideLightbox() })

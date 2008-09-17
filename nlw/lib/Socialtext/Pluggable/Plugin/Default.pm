ecae61c2544d4c4630906e9fd796aa0ba6740049
Graham TerMarsch <graham.termarsch@socialtext.com>
2008-09-17 23:20:41 +0000

    Change behaviour for default "/" redirection: * if user is logged in, show them the "Workspace List" * otherwise, show them the login page (with embedded Public Workspaces List)
    git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/trunk@10095 593ce9bc-2028-0410-bb98-c566b643a948

--- nlw/lib/Socialtext/Pluggable/Plugin/Default.pm
+++ nlw/lib/Socialtext/Pluggable/Plugin/Default.pm
@@ -3,7 +3,6 @@ package Socialtext::Pluggable::Plugin::Default;
 use strict;
 use warnings;
 
-use Socialtext::Rest::NoWorkspace;
 use base 'Socialtext::Pluggable::Plugin';
 use Class::Field 'const';
 const priority => 0;
@@ -19,8 +18,16 @@ sub register {
 
 sub root {
     my ($self, $rest) = @_;
-    my $nowork = Socialtext::Rest::NoWorkspace->new($rest);
-    return $nowork->handler($rest);
+
+    # logged in users go to the Workspace List
+    my $user = $rest->user();
+    if ($user and not $user->is_guest) {
+        return $self->redirect( 'action=workspace_list' );
+    }
+
+    # everyone else goes to the login page (with embedded public Workspace
+    # List)
+    return $self->redirect( '/nlw/login.html' );
 }
 
 sub user_name {

eee6c9b1f8b8e8e348b89536c8f683d3463a7337
Audrey Tang <audrey.tang@socialtext.com>
2008-10-01 08:32:31 +0000

    * Visual Test for {bz: 1322}.
    git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/trunk@10367 593ce9bc-2028-0410-bb98-c566b643a948

---
+++ nlw/share/skin/js-test/widgets/t/bz-1322.t.js
@@ -0,0 +1,33 @@
+(function($) {
+
+var t = new Test.Visual();
+
+t.plan(1);
+
+t.runAsync([
+    function() {
+        $.ajax({
+            url: "/?action=elar_widgets",
+            async: false
+        });
+        $.ajax({
+            url: "/?action=add_widget;location=widgets/share/widgets/recent_conversations.xml",
+            async: false
+        });
+        t.open_iframe("/", t.nextStep(), {w: 600});
+    },
+            
+    function() { 
+        var headerTop = t.$('.widgetHeader').offset().top;
+        var titleTop  = t.$('.widgetHeaderTitleBox').offset().top;
+
+        t.ok(
+            ((titleTop - headerTop) < 10),
+            'Header text should not drop when oversized'
+        );
+
+        t.endAsync();
+    }
+]);
+
+})(jQuery);

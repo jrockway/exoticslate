075f8ed9a1fd29615c495a1e4a61eccd7900617e
Kevin Jones <kevin.jones@socialtext.com>
2008-10-02 21:13:56 +0000

    git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/trunk@10423 593ce9bc-2028-0410-bb98-c566b643a948

--- nlw/lib/Socialtext/Rest/Collection.pm
+++ nlw/lib/Socialtext/Rest/Collection.pm
@@ -98,11 +98,14 @@ sub _make_getter {
        Socialtext::Timer->Stop("GET_$content_type");
        if (my $except = $@) {
             if ($except->isa('Socialtext::Exception::Auth')) {
+                $rest->header(-status => HTTP_401_Unauthorized);
                 return $self->not_authorized;
             } elsif ($except->isa('Socialtext::Exception::NoSuchWorkspace')) {
+                $rest->header(-status => HTTP_404_Not_Found);
                 return $self->no_workspace($except->{name});
             }
             else {
+                $rest->header(-status => HTTP_500_Internal_Server_Error);
                 warn "Error in ST::Rest::Collection: $except";
             }
         }

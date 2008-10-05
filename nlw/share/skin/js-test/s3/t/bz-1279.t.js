42b2a5044967f3e46817fc99a9d1ba406684e101
Audrey Tang <audrey.tang@socialtext.com>
2008-10-05 05:30:25 +0000

    * Improve the test to check for attachment filename mentioned in errmsg
    git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/trunk@10463 593ce9bc-2028-0410-bb98-c566b643a948

--- nlw/share/skin/js-test/s3/t/bz-1279.t.js
+++ nlw/share/skin/js-test/s3/t/bz-1279.t.js
@@ -2,11 +2,11 @@
 
 var t = new Test.Visual();
 
-t.plan(1);
+t.plan(2);
 
 t.runAsync([
     function() {
-        t.open_iframe("/data/workspaces/admin/attachments/admin_wiki:0-0-0/original/bad", t.nextStep());
+        t.open_iframe("/data/workspaces/admin/attachments/admin_wiki:0-0-0/original/VeryBad", t.nextStep());
     },
             
     function() { 
@@ -14,6 +14,12 @@ t.runAsync([
             t.doc.body.innerHTML,
             /Carp/,
             "Invalid attachment URL should not lead to ugly error message"
+        );
+
+        t.like(
+            t.doc.body.innerHTML,
+            /VeryBad/,
+            "The error message should contain the attachment file name"
         );
 
         t.endAsync();

28ef5384e9795add939d1d8ee9b9b6407619050e
Audrey Tang <audrey.tang@socialtext.com>
2008-10-06 10:09:01 +0000

    * Visual Test for {bz: 1427}
    git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/trunk@10469 593ce9bc-2028-0410-bb98-c566b643a948

---
+++ nlw/share/skin/js-test/widgets/t/bz-1427.t.js
@@ -0,0 +1,43 @@
+(function($) {
+
+var t = new Test.Visual();
+
+t.plan(1);
+
+if (jQuery.browser.msie)
+    t.skipAll("Skipping this insanity on IE for now...");
+
+t.runAsync([
+    function() {
+        t.login({}, t.nextStep());
+    },
+
+    function() {
+        var widget = WID = t.setup_one_widget(
+            {
+                url: "/?action=add_widget;location=widgets/share/widgets/tag_cloud.xml",
+                noPoll: true
+            },
+            t.nextStep()
+        );
+    },
+
+    function(widget) {
+        t.scrollTo(150);
+        t.$('a.settings').click();
+        t.$('div.widgetContent select option:first').text(
+            "VeryLongNameVeryLongNameVeryLongNameVeryLongName" +
+            "VeryLongNameVeryLongNameVeryLongNameVeryLongName" +
+            "VeryLongNameVeryLongNameVeryLongNameVeryLongName" +
+            "VeryLongNameVeryLongNameVeryLongNameVeryLongName"
+        );
+        t.ok(
+            (t.$('ul#leftList').width() >= t.$('div.widgetContent select').width()),
+            "Long workspace names should not cause overflow"
+        );
+
+        t.endAsync();
+    }
+]);
+
+})(jQuery);

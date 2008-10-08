600ce7ddbefe1d787b64c1ce505272b603327f15
Audrey Tang <audrey.tang@socialtext.com>
2008-10-08 08:13:33 +0000

    * Visual Test for {bz: 1409}.
    git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/trunk@10518 593ce9bc-2028-0410-bb98-c566b643a948

---
+++ nlw/share/skin/js-test/widgets/t/bz-1409.t.js
@@ -0,0 +1,51 @@
+(function($) {
+
+var t = new Test.Visual();
+
+t.plan(1);
+
+if (jQuery.browser.msie)
+    t.skipAll("Skipping this insanity on IE for now...");
+
+t.runAsync([
+    function() {
+        t.login({}, t.nextStep());
+    },
+
+    function() {
+        $.ajax({
+            url: "/data/workspaces/help-en/pages/socialtext_releases_simple_editing/tags/測",
+            type: 'PUT',
+            async: false,
+            cache: false
+        });
+
+        var widget = WID = t.setup_one_widget(
+            {
+                url: "/?action=add_widget;location=widgets/share/widgets/tag_cloud.xml",
+                noPoll: true
+            },
+            t.nextStep()
+        );
+    },
+
+    function(widget) {
+        t.scrollTo(150);
+
+        var found = false;
+        widget.$('a').each(function(){
+            if ($(this).text().match(/測/)) {
+                found = true;
+            }
+        });
+
+        t.ok(
+            found,
+            "Unicode tag names are handled properly"
+        );
+
+        t.endAsync();
+    }
+]);
+
+})(jQuery);

e728442012142d772d17089c2f4c5f50d879f0d4
Audrey Tang <audrey.tang@socialtext.com>
2008-10-09 02:49:57 +0000

    * Visual Test for {bz: 1262}.
    git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/trunk@10540 593ce9bc-2028-0410-bb98-c566b643a948

---
+++ nlw/share/skin/js-test/s3/t/bz-1262.t.js
@@ -0,0 +1,38 @@
+(function($) {
+
+var t = new Test.Visual();
+
+t.plan(1);
+
+var name = "bz_1262_really_long_"
+    + Math.random() + Math.random() + Math.random() + Math.random()
+    + Math.random() + Math.random() + Math.random() + Math.random();
+
+t.runAsync([
+    function() {
+        t.put_page({
+            workspace: 'admin',
+            page_name: name,
+            content: name,
+            callback: t.nextStep()
+        });
+    },
+
+    function() {
+        t.open_iframe(
+            "/admin/index.cgi?" + name + "#edit",
+            t.nextStep(3000)
+        );
+    },
+            
+    function() { 
+        t.elements_do_not_overlap(
+            t.$('#st-editing-title'),
+            t.$('#st-edit-mode-toolbar'),
+            "Overlong page names should truncate, not overlapping edit toolbar"
+        );
+        t.endAsync();
+    }
+]);
+
+})(jQuery);

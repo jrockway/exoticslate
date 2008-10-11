jrockway/exoticslate

commit 0dc6b93be44384b7ebd5f9b1a8a12281f86282a3

Migrate "User" to user_detail, rectifying the "user_id" confusion in the process.
This is part of [Story: Cache LDAP User Data].

The SQL migration converts users from having weird "user_id"s which aren't system_unique_ids
* Everywhere you see "user_id" really means "system_unique_id" now.
* less confusion = win
* [Migration 2008-10-17] has been updated

When creating a new user on the system:
# get a new SystemUniqueId
# create the homunculus
# create the "UserId" object
# create the "UserMetadata" object

I look forward to the day where we don't have to store our users in 3 separate tables.

Refactored some stale copypasta in the user unit tests while fixing for the new user-creation behaviour.

-- gash

git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/trunk@10505 593ce9bc-2028-0410-bb98-c566b643a948

nlw/lib/Socialtext/User/Default/Factory.pm
@@ -21,7 +21,7 @@ use Email::Valid;
 use Socialtext::User::Cache;
 use Socialtext::Data;
 use Socialtext::String;
-use Socialtext::SQL 'sql_execute';
+use Socialtext::SQL qw(sql_execute);
 use Socialtext::User;
 use Socialtext::UserId;
 use Socialtext::UserMetadata;
@@ -39,6 +39,37 @@ sub driver_name { 'Default' }
 sub driver_key { shift->driver_name }
 
 # FIXME: This belongs elsewhere, in fixture generation code, perhaps
+sub _create_default_user {
+    my $self = shift;
+    my ($username, $email, $created_by) = @_;
+
+    my $id = Socialtext::UserId->SystemUniqueId();
+
+    my $system_unique_id = Socialtext::UserId->create(
+        system_unique_id => $id,
+        driver_key       => $self->driver_key,
+        driver_unique_id => $id,
+        driver_username  => $username,
+    );
+    my $system_user = $self->create(
+        system_unique_id => $id,
+        username         => $username,
+        email_address    => $email,
+        first_name       => 'System',
+        last_name        => 'User',
+        password         => '*no-password*',
+        no_crypt         => 1,
+    );
+    Socialtext::UserMetadata->create(
+        user_id            => $id,
+        created_by_user_id => $created_by,
+        is_system_created  => 1,
+        primary_account_id => Socialtext::Account->Socialtext->account_id(),
+    );
+
+    return $id;
+}
+
 sub EnsureRequiredDataIsPresent {
     my $class = shift;
 
@@ -46,48 +77,18 @@ sub EnsureRequiredDataIsPresent {
     my $factory = $class->new();
 
     # set up the required data
-    unless ( $factory->GetUser( username => $SystemUsername ) ) {
-        my $system_user = $factory->create(
-            username      => $SystemUsername,
-            email_address => $SystemEmailAddress,
-            first_name    => 'System',
-            last_name     => 'User',
-            password      => '*no-password*',
-            no_crypt      => 1,
-        );
-        my $system_unique_id = Socialtext::UserId->create(
-            driver_key       => $class->driver_key,
-            driver_unique_id => $system_user->user_id,
-            driver_username  => $SystemUsername,
-            )->system_unique_id;
-        Socialtext::UserMetadata->create(
-            user_id            => $system_unique_id,
-            created_by_user_id => undef,
-            is_system_created  => 1,
-            primary_account_id => Socialtext::Account->Socialtext->account_id(),
+    unless ($factory->GetUser(username => $SystemUsername)) {
+        $factory->_create_default_user(
+            $SystemUsername, $SystemEmailAddress,
+            undef
         );
     }
 
-    unless ( $factory->GetUser( username => $GuestUsername ) ) {
-        my $system_user = Socialtext::User->new( username => $SystemUsername );
-        my $guest_user = $factory->create(
-            username      => $GuestUsername,
-            email_address => $GuestEmailAddress,
-            first_name    => 'Guest',
-            last_name     => 'User',
-            password      => '*no-password*',
-            no_crypt      => 1,
-        );
-        my $system_unique_id = Socialtext::UserId->create(
-            driver_key       => $class->driver_key,
-            driver_unique_id => $guest_user->user_id,
-            driver_username  => $GuestUsername,
-            )->system_unique_id;
-        Socialtext::UserMetadata->create(
-            user_id            => $system_unique_id,
-            created_by_user_id => $system_user->user_id,
-            is_system_created  => 1,
-            primary_account_id => Socialtext::Account->Socialtext->account_id(),
+    unless ($factory->GetUser(username => $GuestUsername)) {
+        my $system_user = Socialtext::User->new(username => $SystemUsername);
+        $factory->_create_default_user(
+            $GuestUsername, $GuestEmailAddress,
+            $system_user->user_id
         );
     }
 }
@@ -122,7 +123,7 @@ sub new {
 sub Count {
     my ( $self, %p ) = @_;
 
-    my $sth = sql_execute('SELECT COUNT(*) FROM "User"');
+    my $sth = sql_execute('SELECT COUNT(*) FROM user_detail');
     return $sth->fetchall_arrayref->[0][0];
 }
 
@@ -164,7 +165,7 @@ sub _new_from_where {
     my $sth = sql_execute(
         'SELECT user_id, username, email_address,'
         . ' first_name, last_name, password'
-        . ' FROM "User"'
+        . ' FROM user_detail'
         . " WHERE $where_clause=?",
         @bindings
     );
@@ -189,20 +190,24 @@ sub create {
     $p{first_name} ||= '';
     $p{last_name} ||= '';
 
-    sql_execute(
-        'INSERT INTO "User"'
-        . ' (user_id, username, email_address, first_name, last_name, password)'
-        . ' VALUES (nextval(\'"User___user_id"\'),?,?,?,?,?)',
-        $p{username}, $p{email_address}, $p{first_name}, $p{last_name},
-        $p{password}
+    sql_execute( q{
+            INSERT INTO user_detail
+            (user_id, username, email_address, first_name, last_name, password)
+            VALUES (?,?,?,?,?,?)
+        }, $p{system_unique_id}, $p{username},  $p{email_address},
+        $p{first_name}, $p{last_name}, $p{password}
     );
 
-    return $self->GetUser( username => $p{username} );
+    # flush cache; added a User to the DB
+    Socialtext::User::Cache->Clear();
+
+    return $self->GetUser(username => $p{username});
 }
 
 sub delete {
     my ( $self, $user ) = @_;
-    my $sth = sql_execute( 'DELETE FROM "User" WHERE user_id=?', $user->user_id );
+    my $sth = sql_execute('DELETE FROM user_detail WHERE user_id=?', 
+                          $user->user_id);
 
     # flush cache; removed a User from the DB
     Socialtext::User::Cache->Clear();
@@ -225,7 +230,7 @@ sub update {
     my $set_clause = join ', ', @updates;
 
     sql_execute(
-        'UPDATE "User"'
+        'UPDATE user_detail'
         . " SET $set_clause WHERE user_id=?",
         @bindings, $user->user_id);
 
@@ -254,7 +259,7 @@ sub Search {
 
     my $sth = sql_execute(
         'SELECT first_name, last_name, email_address'
-        . ' FROM "User" WHERE'
+        . ' FROM user_detail WHERE'
         . ' ( LOWER( username ) LIKE ? OR'
         . ' LOWER( email_address ) LIKE ? OR'
         . ' LOWER( first_name ) LIKE ? OR'
@@ -288,66 +293,77 @@ sub _validate_and_clean_data {
 
     my $is_create = defined $user ? 0 : 1;
 
-    if (not $is_create) {
-        my $system_unique_id = Socialtext::UserId->new(
-            driver_key       => $self->driver_key,
-            driver_unique_id => $user->user_id
-        )->system_unique_id;
+    if ($is_create) {
+        die "must have pre-specified a system_unique_id to use"
+            unless $p->{system_unique_id};
+    }
+    else {
         $metadata = Socialtext::UserMetadata->new(
-            user_id => $system_unique_id
+            user_id => $user->user_id
         );
     }
 
     my @errors;
-    for my $k ( qw( username email_address ) ) {
+    for my $k (qw(username email_address)) {
         $p->{$k} = Socialtext::String::trim( lc $p->{$k} )
             if defined $p->{$k};
 
-        if ( defined $p->{$k}
-             and ( $is_create
-                   or $p->{$k} ne $user->$k() )
-             and Socialtext::User->new_homunculus( $k => $p->{$k} ) ) {
+        if (defined $p->{$k}
+            and (  $is_create
+                or $p->{$k} ne $user->$k())
+            and Socialtext::User->new_homunculus($k => $p->{$k}))
+        {
             push @errors, loc("The [_1] you provided ([_2]) is already in use.", Socialtext::Data::humanize_column_name($k), $p->{$k});
         }
 
-        if ( ( exists $p->{$k} or $is_create )
-             and not
-             ( defined $p->{$k} and length $p->{$k} ) ) {
+        if ((exists $p->{$k} or $is_create)
+            and not(defined $p->{$k} and length $p->{$k}))
+        {
             push @errors,
-                    loc('[_1] is a required field.', ucfirst Socialtext::Data::humanize_column_name($k));
+                    loc('[_1] is a required field.', 
+                        ucfirst Socialtext::Data::humanize_column_name($k));
 
         }
     }
 
-    if ( defined $p->{email_address} && length $p->{email_address}
-         && ! Email::Valid->address( $p->{email_address} ) ) {
-        push @errors, loc("[_1] is not a valid email address.",$p->{email_address});
+    if (defined $p->{email_address}
+        and length $p->{email_address}
+        and !Email::Valid->address($p->{email_address}))
+    {
+        push @errors,
+            loc(
+            "[_1] is not a valid email address.",
+            $p->{email_address}
+            );
     }
 
-    if ( defined $p->{password} && length $p->{password} < 6 ) {
-        push @errors, Socialtext::User::Default->ValidatePassword( password => $p->{password} );
+    if (defined $p->{password} && length $p->{password} < 6) {
+        push @errors, Socialtext::User::Default->ValidatePassword(
+            password => $p->{password});
     }
 
-    if ( delete $p->{require_password}
-         and $is_create and not defined $p->{password} ) {
+    if (delete $p->{require_password}
+        and $is_create
+        and not defined $p->{password})
+    {
         push @errors, loc('A password is required to create a new user.');
     }
 
-    if ( not $is_create and $metadata ) {
-        if ( $metadata->is_system_created ) {
-            push @errors,
-                loc("You cannot change the name of a system-created user.")
-                if $p->{username};
+    if (!$is_create and $metadata and $metadata->is_system_created) {
+        push @errors,
+            loc("You cannot change the name of a system-created user.")
+            if $p->{username};
 
-            push @errors,
-                loc("You cannot change the email address of a system-created user.")
-                if $p->{email_address};
-        }
+        push @errors,
+            loc("You cannot change the email address of a system-created user.")
+            if $p->{email_address};
     }
 
     data_validation_error errors => \@errors if @errors;
 
-    if ( $is_create and not ( defined $p->{password} and length $p->{password} ) ) {
+    if ($is_create
+        and not(defined $p->{password} and length $p->{password}))
+    {
         $p->{password} = '*none*';
         $p->{no_crypt} = 1;
     }

nlw/etc/socialtext/db/dev.sql
@@ -11,8 +11,8 @@ CREATE OR REPLACE VIEW xpage AS
             JOIN "Workspace" ON (page.workspace_id = "Workspace".workspace_id) 
             JOIN "UserId" editorid  ON (page.last_editor_id = editorid.system_unique_id) 
             JOIN "UserId" creatorid  ON (page.creator_id = creatorid.system_unique_id) 
-            LEFT JOIN "User" editor  ON (editorid.driver_unique_id = editor.user_id) 
-            LEFT JOIN "User" creator ON (creatorid.driver_unique_id = creator.user_id);
+            LEFT JOIN user_detail editor  ON (editorid.driver_unique_id = editor.user_id) 
+            LEFT JOIN user_detail creator ON (creatorid.driver_unique_id = creator.user_id);
 
 CREATE OR REPLACE VIEW xpage_tag AS 
     SELECT "Workspace".name AS workspace_name, page_tag.*
@@ -25,14 +25,14 @@ CREATE OR REPLACE VIEW xworkspace AS
             JOIN "Account" ON ("Workspace".account_id = "Account".account_id);
 
 CREATE OR REPLACE VIEW xuwr AS 
-    SELECT "User".username, 
+    SELECT user_detail.username, 
            "Workspace".name AS workspace_name,
            "Role".name AS role_name
         FROM "UserWorkspaceRole" uwr
             JOIN "Workspace" ON (uwr.workspace_id = "Workspace".workspace_id)
             JOIN "Role" ON (uwr.role_id = "Role".role_id)
             JOIN "UserId" ON (uwr.user_id = "UserId".system_unique_id)
-            LEFT JOIN "User" ON ("UserId".driver_unique_id = "User".user_id);
+            LEFT JOIN user_detail ON ("UserId".driver_unique_id = user_detail.user_id);
 
 CREATE OR REPLACE VIEW xevent AS
     SELECT e.at AS at, 
@@ -46,9 +46,9 @@ CREATE OR REPLACE VIEW xevent AS
            e.context AS context
     FROM event e 
          LEFT JOIN "UserId" actorid ON (e.actor_id = actorid.system_unique_id)
-         LEFT JOIN "User" actor ON (actorid.driver_unique_id = actor.user_id) 
+         LEFT JOIN user_detail actor ON (actorid.driver_unique_id = actor.user_id) 
          LEFT JOIN "UserId" personid ON (e.person_id = personid.system_unique_id)
-         LEFT JOIN "User" person ON (personid.driver_unique_id = person.user_id) 
+         LEFT JOIN user_detail person ON (personid.driver_unique_id = person.user_id) 
          LEFT JOIN page p 
             ON (e.page_workspace_id = p.workspace_id AND e.page_id = p.page_id) 
          LEFT JOIN "Workspace" w 

nlw/etc/socialtext/db/socialtext-16-to-17.sql
@@ -1,0 +1,47 @@
+BEGIN;
+
+CREATE TABLE user_detail (
+    user_id bigint NOT NULL PRIMARY KEY,
+    username text NOT NULL,
+    email_address text NOT NULL,
+    "password" text NOT NULL,
+    first_name text DEFAULT ''::text NOT NULL,
+    last_name text DEFAULT ''::text NOT NULL,
+    cached_at timestamptz
+);
+
+\x
+\o /tmp/user_detail_migration.txt
+SELECT * FROM "User";
+\o
+\x
+
+-- migrate 'Default' users into the user_detail table
+INSERT INTO user_detail 
+(user_id, username, email_address, password,
+ first_name, last_name, cached_at)
+SELECT uid.system_unique_id::bigint,
+    u.username::text,
+    u.email_address::text,
+    u.password::text,
+    first_name::text,
+    last_name::text,
+    NULL::timestamptz
+FROM "UserId" uid 
+JOIN "User" u ON (uid.driver_unique_id = u.user_id)
+WHERE uid.driver_key = 'Default';
+
+-- recreate former "User" table indexes
+CREATE UNIQUE INDEX "user_detail_lower_email_address"
+	    ON user_detail (lower((email_address)::text));
+
+CREATE UNIQUE INDEX "user_detail_lower_username"
+	    ON user_detail (lower((username)::text));
+
+DROP TABLE "User" CASCADE;
+
+UPDATE "System"
+   SET value = 17
+ WHERE field = 'socialtext-schema-version';
+
+COMMIT;

nlw/etc/socialtext/db/socialtext-schema.sql
@@ -82,15 +82,6 @@ CREATE TABLE "System" (
     last_update timestamptz DEFAULT now()
 );
 
-CREATE TABLE "User" (
-    user_id bigint NOT NULL,
-    username varchar(250) NOT NULL,
-    email_address varchar(250) NOT NULL,
-    "password" varchar(40) NOT NULL,
-    first_name varchar(200) DEFAULT ''::varchar NOT NULL,
-    last_name varchar(200) DEFAULT ''::varchar NOT NULL
-);
-
 CREATE TABLE "UserEmailConfirmation" (
     user_id bigint NOT NULL,
     sha1_hash varchar(27) NOT NULL,
@@ -349,6 +340,16 @@ CREATE VIEW user_account AS
    LEFT JOIN "Workspace" w ON uwr.workspace_id = w.workspace_id
   ORDER BY u.system_unique_id, u.driver_key, u.driver_unique_id, u.driver_username, um.created_by_user_id, um.creation_datetime, um.primary_account_id, w.account_id;
 
+CREATE TABLE user_detail (
+    user_id bigint NOT NULL,
+    username text NOT NULL,
+    email_address text NOT NULL,
+    "password" text NOT NULL,
+    first_name text DEFAULT '' NOT NULL,
+    last_name text DEFAULT '' NOT NULL,
+    cached_at timestamptz
+);
+
 ALTER TABLE ONLY "Account"
     ADD CONSTRAINT "Account_pkey"
             PRIMARY KEY (account_id);
@@ -376,10 +377,6 @@ ALTER TABLE ONLY "UserMetadata"
 ALTER TABLE ONLY "UserWorkspaceRole"
     ADD CONSTRAINT "UserWorkspaceRole_pkey"
             PRIMARY KEY (user_id, workspace_id);
-
-ALTER TABLE ONLY "User"
-    ADD CONSTRAINT "User_pkey"
-            PRIMARY KEY (user_id);
 
 ALTER TABLE ONLY "Watchlist"
     ADD CONSTRAINT "Watchlist_pkey"
@@ -445,6 +442,10 @@ ALTER TABLE ONLY tag_people__person_tags
     ADD CONSTRAINT tag_people__person_tags_pkey
             PRIMARY KEY (person_id, tag_id);
 
+ALTER TABLE ONLY user_detail
+    ADD CONSTRAINT user_detail_pkey
+            PRIMARY KEY (user_id);
+
 CREATE UNIQUE INDEX "Account___name"
 	    ON "Account" (name);
 
@@ -468,12 +469,6 @@ CREATE INDEX "UserMetadata_primary_account_id"
 
 CREATE INDEX "UserWorkspaceRole_workspace_id"
 	    ON "UserWorkspaceRole" (workspace_id);
-
-CREATE UNIQUE INDEX "User___lower___email_address"
-	    ON "User" (lower((email_address)::text));
-
-CREATE UNIQUE INDEX "User___lower___username"
-	    ON "User" (lower((username)::text));
 
 CREATE UNIQUE INDEX "Workspace___lower___name"
 	    ON "Workspace" (lower((name)::text));
@@ -552,6 +547,12 @@ CREATE INDEX storage_key_value_type_ix
 CREATE INDEX storage_key_value_viewer_ix
 	    ON "storage" ("key", value)
 	    WHERE (("key")::text = 'viewer');
+
+CREATE UNIQUE INDEX user_detail_lower_email_address
+	    ON user_detail (lower(email_address));
+
+CREATE UNIQUE INDEX user_detail_lower_username
+	    ON user_detail (lower(username));
 
 CREATE INDEX watchlist_user_workspace
 	    ON "Watchlist" (user_id, workspace_id);
@@ -722,4 +723,4 @@ ALTER TABLE ONLY "Workspace"
             REFERENCES "Account"(account_id) ON DELETE CASCADE;
 
 DELETE FROM "System" WHERE field = 'socialtext-schema-version';
-INSERT INTO "System" VALUES ('socialtext-schema-version', '16');
+INSERT INTO "System" VALUES ('socialtext-schema-version', '17');

nlw/lib/Socialtext/User.pm
@@ -167,38 +167,41 @@ sub new {
     return $user;
 }
 
+
+
 sub create {
     my $class = shift;
 
     # username email_address password first_name last_name
     my %p = @_;
+    my $id = Socialtext::UserId->SystemUniqueId();
+    $p{system_unique_id} = $id;
+
     my $homunculus = $class->_first( 'create', %p );
 
     my $system_unique_id = Socialtext::UserId->create(
+        system_unique_id => $id,
         driver_key       => $homunculus->driver_key,
-        driver_unique_id => $homunculus->user_id,
+        driver_unique_id => $id,
         driver_username  => $homunculus->username,
-    )->system_unique_id();
+    );
 
-    if ( !exists $p{created_by_user_id} ) {
-        if ( $homunculus->username ne $SystemUsername ) {
-            my $s_u_homunculus = $class->new_homunculus( username => $SystemUsername );
-            my $driver_key = $s_u_homunculus->driver_key;
-            my $driver_unique_id = $s_u_homunculus->user_id;
-            $p{created_by_user_id} = Socialtext::UserId->new(
-                driver_key       => $driver_key,
-                driver_unique_id => $driver_unique_id
-            )->system_unique_id;
+    if (!exists $p{created_by_user_id}) {
+        if ($homunculus->username ne $SystemUsername) {
+            $p{created_by_user_id} = Socialtext::User->SystemUser()->user_id;
         }
     }
+
+
     my $user = bless {}, $class;
-    $user->homunculus( $homunculus );
+    $user->homunculus($homunculus);
+
     # scribble UserMetadata
-    my %metadata_p = map { $_ => $p{$_} } keys %p; #@user_metadata_interface;;
-    $metadata_p{user_id}                 = $system_unique_id;
+    my %metadata_p = %p; # copy
+    $metadata_p{user_id} = $id;
     $metadata_p{email_address_at_import} = $user->email_address;
     my $metadata = Socialtext::UserMetadata->create(%metadata_p);
-    $user->metadata( $metadata );
+    $user->metadata($metadata);
 
     return $user;
 }

nlw/lib/Socialtext/UserId.pm
@@ -8,7 +8,7 @@ our $VERSION = '0.01';
 
 use Class::Field 'field';
 use Socialtext::Cache;
-use Socialtext::SQL qw( sql_execute );
+use Socialtext::SQL qw(sql_execute sql_singlevalue);
 
 field 'system_unique_id';
 field 'driver_key';
@@ -29,24 +29,28 @@ sub create_if_necessary {
         driver_username => $homunculus->username,
     );
 
-    my $user_id = $class->new( @primary_params );
+    my $user_id = $class->_new_from_homunculus(@primary_params);
 
-    if (! defined $user_id) {
-        $user_id = $class->new( @fallback_params );
+    if (!defined $user_id) {
+        $user_id = $class->_new_from_homunculus(@fallback_params);
     }
 
     if ($user_id) {
-        if( $user_id->_homunculus_is_different( $homunculus ) ) {
+        if ($user_id->_homunculus_is_different($homunculus)) {
             $user_id->update(
-                             driver_unique_id => $homunculus->user_id,
-                             driver_username  => $homunculus->username
-                            );
+                driver_unique_id => $homunculus->user_id,
+                driver_username  => $homunculus->username
+            );
         }
         return $user_id;
     }
     
-    return $class->create( @primary_params,
-        driver_username => $homunculus->username );
+    my $new_id = $class->SystemUniqueId();
+    return $class->create( 
+        @primary_params,
+        driver_username => $homunculus->username,
+        system_unique_id => $new_id
+    );
 }
 
 sub _homunculus_is_different {
@@ -141,20 +145,28 @@ sub _new_from_where {
     return @rows ? bless {
                     system_unique_id => $rows[0][0],
                     driver_key       => $rows[0][1],
-                    driver_unique_id => $rows[0][2],
+                   driver_unique_id => $rows[0][2],
                     driver_username  => $rows[0][3],
                     }, $class
                  : undef;
 }
 
+sub SystemUniqueId {
+    my $id = sql_singlevalue(q{SELECT nextval('"UserId___system_unique_id"')});
+    return $id;
+}
+
 sub create {
     my ( $class, %p ) = @_;
+
+    die "need to supply a system_unique_id; use GetUniqueId"
+        unless $p{system_unique_id};
 
     sql_execute(
         'INSERT INTO "UserId"'
         . ' (system_unique_id, driver_key, driver_unique_id, driver_username)'
-        . ' VALUES (nextval(\'"UserId___system_unique_id"\'),?,?,?)',
-        $p{driver_key}, $p{driver_unique_id}, $p{driver_username} );
+        . ' VALUES (?,?,?,?)',
+        $p{system_unique_id}, $p{driver_key}, $p{driver_unique_id}, $p{driver_username} );
 
     return $class->new(%p);
 }
@@ -245,14 +257,21 @@ PARAMS can include:
 
 =back
 
+=head2 Socialtext::UserId->SystemUniqueId()
+
+Returns a new unique identifier for use in creating new users.
+
 =head2 Socialtext::UserId->create(PARAMS)
 
 Attempts to create a user metadata record with the given information and
-returns a new C<Socialtext>::UserId object.
+returns a new C<Socialtext>::UserId object.  Be sure to call SystemUniqueId to
+get a new system_unique_id to pass in.
 
 PARAMS can include:
 
 =over 4
+
+=item * system_unique_id - required
 
 =item * driver_key - required
 

nlw/t/Socialtext/User-deleted.t
@@ -25,7 +25,9 @@ use Socialtext::User::Default::Factory;
 
 # populate a user that doesn't currently exist, even though it uses a
 # known driver.
+my $unique_id = Socialtext::UserId->SystemUniqueId();
 my $user_id = Socialtext::UserId->create(
+    system_unique_id => $unique_id,
     driver_key       => 'Default',
     driver_unique_id => 999999,
     driver_username  => "Nemo",
@@ -72,11 +74,13 @@ my $user_id = Socialtext::UserId->create(
 Default_username: {
     # Create a UserId, but no User, to trick the system into thinking
     # this is a deleted user.
-    my $id = Socialtext::UserId->create(
+    my $id = Socialtext::UserId->SystemUniqueId();
+    Socialtext::UserId->create(
+        system_unique_id => $id,
         driver_key       => 'Default',
         driver_unique_id => "Rubber Bands",
         driver_username  => '',
-    )->system_unique_id;
+    );
 
     my $nemo = Socialtext::User->new( user_id => $id );
     ok ($nemo->to_hash, "deleted user can be hashified" );

commit d1a852aa639b45883063cadf2c2bc0a8dd0481c6

Create default users with their proper first/last names (like we used to).
Fixes a regression from r10505.


git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/trunk@10576 593ce9bc-2028-0410-bb98-c566b643a948

nlw/lib/Socialtext/User/Default/Factory.pm
@@ -41,7 +41,7 @@ sub driver_key { shift->driver_name }
 # FIXME: This belongs elsewhere, in fixture generation code, perhaps
 sub _create_default_user {
     my $self = shift;
-    my ($username, $email, $created_by) = @_;
+    my ($username, $email, $first_name, $created_by) = @_;
 
     my $id = Socialtext::UserId->NewUserId();
 
@@ -55,7 +55,7 @@ sub _create_default_user {
         user_id       => $id,
         username      => $username,
         email_address => $email,
-        first_name    => 'System',
+        first_name    => $first_name,
         last_name     => 'User',
         password      => '*no-password*',
         no_crypt      => 1,
@@ -80,7 +80,7 @@ sub EnsureRequiredDataIsPresent {
     unless ($factory->GetUser(username => $SystemUsername)) {
         $factory->_create_default_user(
             $SystemUsername, $SystemEmailAddress,
-            undef
+            'System', undef
         );
     }
 
@@ -88,7 +88,7 @@ sub EnsureRequiredDataIsPresent {
         my $system_user = Socialtext::User->new(username => $SystemUsername);
         $factory->_create_default_user(
             $GuestUsername, $GuestEmailAddress,
-            $system_user->user_id
+            'Guest', $system_user->user_id
         );
     }
 }
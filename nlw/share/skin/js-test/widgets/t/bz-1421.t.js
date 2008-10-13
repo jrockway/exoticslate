fc52d6369c7cf0df3293dcf425cf8b4905a5d5f9
Audrey Tang <audrey.tang@socialtext.com>
2008-10-13 22:55:13 +0000

    * Proper & Comprehensive Visual Test for {bz: 1421}.
    git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/trunk@10603 593ce9bc-2028-0410-bb98-c566b643a948

--- nlw/share/skin/js-test/widgets/t/bz-1421.t.js
+++ nlw/share/skin/js-test/widgets/t/bz-1421.t.js
@@ -2,7 +2,7 @@
 
 var t = new Test.Visual();
 
-t.plan(1);
+t.plan(4);
 
 if (jQuery.browser.msie)
     t.skipAll("Skipping this insanity on IE for now...");
@@ -24,16 +24,38 @@ t.runAsync([
 
     function() {
         t.scrollTo(150);
+
+        t.is(
+            t.$('div.widget.minimized').length,
+            0,
+            "Widgets start off non-minimized"
+        );
+
         t.$('div.widgetHeader a.minimize:first').click();
+
+        t.is(
+            t.$('div.widget.minimized').length,
+            1,
+            "Clicking 'minimize' causes minimization"
+        );
+
         t.iframe.contentWindow.location = '/index.cgi?';
         t.callNextStep(3000);
     },
 
     function() {
         t.is(
-            t.$('div.widgetContent div.visible').length,
-            0,
+            t.$('div.widget.minimized').length,
+            1,
             "Minimizing a widget should persist across reloads"
+        );
+
+        t.$('div.widgetHeader a.minimize:first').click();
+
+        t.is(
+            t.$('div.widget.minimized').length,
+            0,
+            "Clicking 'minimize' again stops minimization"
         );
 
         t.endAsync();

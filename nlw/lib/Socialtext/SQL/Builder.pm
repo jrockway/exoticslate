8b2f77705b8b36bb2bd58e4a8f87890a7630a32d
Jeremy Stashewsky <jeremy.stashewsky@socialtext.com>
2008-10-16 03:43:13 +0000

    Add ST::SQL::Builder, an SQL generator for simple inserts/updates.
    ST::SQL::Builder does not quote identifiers; table and column names should be
    de-tainted before using sql_insert and sql_update.
    
    This maybe overlaps with SQL::Abstract on cpan a little bit, but is much
    simpler.  SELECT statements should be done by hand IMHO anyway.
    
    
    git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/trunk@10643 593ce9bc-2028-0410-bb98-c566b643a948

---
+++ nlw/lib/Socialtext/SQL/Builder.pm
@@ -0,0 +1,133 @@
+package Socialtext::SQL::Builder;
+# @COPYRIGHT@
+use strict;
+use base 'Exporter';
+use Socialtext::SQL qw(:exec);
+use Carp qw/croak cluck/;
+
+our @EXPORT = ();
+our @EXPORT_OK = qw(
+    sql_nextval
+    sql_update sql_insert
+);
+our %EXPORT_TAGS = (
+    'all' => \@EXPORT_OK,
+);
+
+=head1 NAME
+
+Socialtext::SQL::Builder - code for generating common SQL statements
+
+=head1 SYNOPSIS
+
+  use Socialtext::SQL::Builder qw(:all)
+
+  my %insert_or_update = (
+      name => 'Carl',
+      addr => 'foo@example.com',
+  );
+
+  $insert_or_update{id_field} = sql_nextval('some_sequence_name');
+
+  # does a SQL "INSERT INTO tablename ..."
+  sql_insert('tablename' => \%insert_or_update);
+
+  $insert_or_update{name} = 'Steve';
+  $insert_or_update{addr} = 'bar@example.com';
+
+  # does a SQL "UPDATE tablename SET ... WHERE id_field = ?"
+  sql_update('tablename' => \%insert_or_update, 'id_field');
+
+=head1 DESCRIPTION
+
+Provides convenience methods on top of what C<Socialtext::SQL> provides.
+
+The generated SQL is specific to C<DBD::Pg>.
+
+=head1 METHODS
+
+=head2 sql_nextval($sequence)
+
+Get the next value of the named sequence.
+
+=cut
+
+sub sql_nextval {
+    return sql_singlevalue("SELECT nextval(?)", shift);
+}
+
+=head2 sql_update($table, $hashref, $pk_key)
+
+Update the specified table with the provided hash of values.  C<$pk_key> is a
+key into the hash of values that is to be used as a unique identifier in the
+WHERE clause.
+
+One "SET" statement will be generated for each key-value pair in the hashref
+except for the C<$pk_key> field, which cannot be updated with this function.
+
+B<Caution:> No validation is done on the table name or the keys in the hashref.
+
+Currently tables with composite unique keys are not supported.
+
+=cut
+
+sub sql_update {
+    my $table = shift;
+    my $p = shift;
+    my $pk_key = shift;
+
+    die "no table name" unless $table;
+    die "invalid key name" unless $pk_key;
+    die "nothing to update" unless ($p and %$p);
+
+    my $pk_val = $p->{$pk_key};
+    die "no pk value" unless defined $pk_val;
+
+    my @keys = sort grep {$_ ne $pk_key} keys %$p;
+    my $set_params = join(', ', map {"$_ = ?"} @keys);
+
+    my $sql = "UPDATE $table SET $set_params WHERE $pk_key = ?";
+
+    local $Socialtext::SQL::Level = $Socialtext::SQL::Level + 1;
+    return sql_execute($sql, (map {$p->{$_}} @keys), $pk_val);
+}
+
+=head2 sql_insert ($table, $hashref)
+
+Insert into the specified table using the provided hash of values.
+
+B<Caution>: No validation is done on the keys or values of the hashref.
+
+=cut
+
+sub sql_insert {
+    my $table = shift;
+    my $p = shift;
+
+    die "no table name" unless $table;
+    die "nothing to update" unless ($p and %$p);
+
+    my @keys = sort keys %$p;
+    my $fields = join(',', @keys);
+    my $placeholders = '?,' x @keys;
+    chop $placeholders;
+
+    my $sql = "INSERT INTO $table ($fields) VALUES ($placeholders)";
+
+    local $Socialtext::SQL::Level = $Socialtext::SQL::Level + 1;
+    return sql_execute($sql, (map {$p->{$_}} @keys));
+}
+
+1;
+__END__
+=head1 SEE ALSO
+
+C<Socialtext::SQL>, C<DBD::Pg>
+
+=head1 AUTHOR
+
+Jeremy Stashewsky <stash@socialtext.com> & the Socialtext crew
+
+=head1 COPYRIGHT
+
+@COPYRIGHT@
---
+++ nlw/t/Socialtext/SQL/Builder.t
@@ -0,0 +1,81 @@
+#!perl
+# @COPYRIGHT@
+use strict;
+use warnings FATAL => 'all';
+
+use Test::More tests => 24;
+use Test::Exception;
+use mocked 'Socialtext::SQL', ':test';
+
+use_ok 'Socialtext::SQL::Builder', ':all';
+
+nextval: {
+    sql_mock_result([42]);
+
+    my $next = sql_nextval('somesequence');
+    is $next, 42, "got expected nextval";
+    sql_ok(
+        sql => "SELECT nextval(?)",
+        args => ['somesequence'],
+        name => 'sql_nextval',
+    );
+}
+
+update_invalid_calls: {
+    dies_ok sub { sql_update('') };
+    dies_ok sub { sql_update(undef) };
+    dies_ok sub { sql_update('mytable') }, 'missing args';
+
+    dies_ok sub { sql_update('mytable', undef, 'id') }, 'missing params';
+    dies_ok sub { sql_update('mytable', {}, 'id') }, 'empty params';
+
+    dies_ok sub { sql_update('mytable', {foo=>12}, 'id') }, 'missing key';
+    dies_ok sub { sql_update('mytable', {foo=>undef}, 'foo') }, 'undef value';
+
+    ok !@Socialtext::SQL::SQL, 'no sql got run';
+}
+
+update_works: {
+    sql_update(
+        'mytable_1',
+        {
+            foo  => 'bar',
+            this => 'that',
+            baz  => 'quxx',
+        },
+        'foo'
+    );
+    sql_ok(
+        sql => q{UPDATE mytable_1 SET baz = ?, this = ? WHERE foo = ?},
+        args => ['quxx', 'that', 'bar'],
+        name => 'sql_update works',
+    );
+    ok !@Socialtext::SQL::SQL, 'no more sql';
+}
+
+insert_invalid_calls: {
+    dies_ok sub { sql_insert('') };
+    dies_ok sub { sql_insert(undef) };
+    dies_ok sub { sql_insert('mytable') }, 'missing args';
+    dies_ok sub { sql_insert('mytable', undef) }, 'missing params';
+    dies_ok sub { sql_insert('mytable', {}) }, 'empty params';
+
+    ok !@Socialtext::SQL::SQL, 'no sql got run';
+}
+
+insert_works: {
+    sql_insert(
+        'mytable_2',
+        {
+            foo2  => 'bar',
+            this2 => 'that',
+            baz2  => 'quxx',
+        },
+    );
+    sql_ok(
+        sql => q{INSERT INTO mytable_2 (baz2,foo2,this2) VALUES (?,?,?)},
+        args => ['quxx', 'bar', 'that'],
+        name => 'sql_insert works',
+    );
+    ok !@Socialtext::SQL::SQL, 'no more sql';
+}

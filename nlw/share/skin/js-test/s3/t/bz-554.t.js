6f5ed0b5a782adbaad770f021a1ed51801963032
Audrey Tang <audrey.tang@socialtext.com>
2008-10-17 13:51:06 +0000

    * Visual Test for {bz: 554}.
    git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/trunk@10662 593ce9bc-2028-0410-bb98-c566b643a948

---
+++ nlw/share/skin/js-test/s3/t/bz-554.t.js
@@ -0,0 +1,90 @@
+(function($) {
+
+var t = new Test.Visual();
+
+t.plan(4);
+
+t.checkRichTextSupport();
+
+t.runAsync([
+    function() {
+        t.put_page({
+            workspace: 'admin',
+            page_name: "bz_554",
+            content: '"<&>"<http://example.org/>\n',
+            callback: t.nextStep()
+        });
+    },
+
+    function() {
+        t.open_iframe("/admin/index.cgi?bz_554", t.nextStep());
+    },
+            
+    function() { 
+        t.is(
+            t.$('div.wiki a').text(),
+            "<&>",
+            "Special chars in link text is recognized correctly (text)"
+        );
+
+        t.is(
+            t.$('div.wiki a').attr('href'),
+            "http://example.org/",
+            "Special chars in link text is recognized correctly (href)"
+        );
+
+        t.put_page({
+            workspace: 'admin',
+            page_name: "bz_554",
+            content: '\n',
+            callback: t.nextStep()
+        });
+
+    },
+
+    function() { 
+        t.$('#st-edit-button-link').click();
+        t.callNextStep(3000);
+    },
+
+    function() { 
+        t.$('#st-mode-wysiwyg-button').click();
+        t.callNextStep(1000);
+    },
+
+    function() { 
+        t.$('#wikiwyg_button_link').click();
+        t.callNextStep(1000);
+    },
+
+    function() { 
+        t.$('#add-web-link').click();
+        t.$('#web-link-text').val('<&>');
+        t.$('#web-link-destination').val('http://example.org/');
+        t.$('#add-a-link-form').submit();
+        t.callNextStep(3000);
+    },
+
+    function() { 
+        t.$('#st-save-button-link').click();
+        t.callNextStep(3000);
+    },
+
+    function() { 
+        t.is(
+            t.$('div.wiki a').text(),
+            "<&>",
+            "Special chars in link text is recognized correctly (text)"
+        );
+
+        t.is(
+            t.$('div.wiki a').attr('href'),
+            "http://example.org/",
+            "Special chars in link text is recognized correctly (href)"
+        );
+
+        t.endAsync();
+    }
+]);
+
+})(jQuery);

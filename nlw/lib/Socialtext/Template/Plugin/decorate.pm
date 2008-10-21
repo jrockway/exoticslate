201b045227ef54d2ebd09d6cc14a55d59b2eb7c7
Audrey Tang <audrey.tang@socialtext.com>
2008-10-21 14:53:31 +0000

    * Memoize decorated results under the same context. Perf: 25sec -> 10sec.   This assumes that repeated calls to [% text | decorate('name') %] will   yield the same result during one rendering of a template.
    git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/trunk@10698 593ce9bc-2028-0410-bb98-c566b643a948

--- nlw/lib/Socialtext/Template/Plugin/decorate.pm
+++ nlw/lib/Socialtext/Template/Plugin/decorate.pm
@@ -16,18 +16,38 @@ sub init {
 }
 
 
+# As long as we're under the same context, memoize the decorated results.
+# This assumes that repeated calls to [% text | decorate('name') %] will
+# yield the same result during one rendering of a template.
+my ($ActiveContext, %DecorateCache);
+
 sub filter {
     my ($self, $text, $args, $config) = @_;
 
     my $pluggable = $self->{_CONTEXT}->stash->get('pluggable');
     my $name = $args->[0];
 
+    my $cache_key = "$name.$text";
+    if ($ActiveContext == 0+$self->{_CONTEXT}) {
+        if (exists $DecorateCache{$cache_key}) {
+            return $DecorateCache{$cache_key};
+        }
+    }
+    else {
+        $ActiveContext = 0+$self->{_CONTEXT};
+        %DecorateCache = ();
+    }
+
     if ($pluggable->registered("template.$name.content")) {
         $text = $pluggable->hook("template.$name.content", $text);
     }
     my $prepend = $pluggable->hook("template.$name.prepend", $text);
     my $append  = $pluggable->hook("template.$name.append", $text);
-    return "${prepend}${text}${append}";
+
+    my $result = "${prepend}${text}${append}";
+    $DecorateCache{$cache_key} = $result;
+
+    return $result;
 }
 
 1;

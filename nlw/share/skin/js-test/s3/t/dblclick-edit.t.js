8b28c57cdeb685dd851bdc715c83eebf42afc166
Audrey Tang <audrey.tang@socialtext.com>
2008-10-25 18:48:38 +0000

    * dblclick-edit.t.js: Modernise the test and don't trigger "navigate away".
    git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/trunk@10798 593ce9bc-2028-0410-bb98-c566b643a948

--- nlw/share/skin/js-test/s3/t/dblclick-edit.t.js
+++ nlw/share/skin/js-test/s3/t/dblclick-edit.t.js
@@ -6,42 +6,54 @@ t.plan(5);
 
 t.runAsync([
     function() {
-        t.open_iframe("/admin/index.cgi?admin_wiki", t.nextStep());
+        t.put_page({
+            workspace: 'admin',
+            page_name: "dblclick-edit",
+            content: "dblclick-edit",
+            callback: t.nextStep()
+        });
+    },
+
+    function() {
+        t.open_iframe("/admin/index.cgi?dblclick-edit", t.nextStep());
     },
 
     function() {
         t.scrollTo(100);
 
-        t.builder.ok(
+        t.ok(
             t.$("div#st-display-mode-container").is(":visible"),
             'Display is visible before edit'
         );
 
-        t.builder.ok(
+        t.ok(
             t.$("div#st-edit-mode-view").is(":hidden") ||
             ( t.$("div#st-edit-mode-view").size() == 0 ),
             'Editor is not visible'
         );
 
-        t.$("div#st-page-content").trigger("dblclick");
+        t.$("div#st-page-content").dblclick();
 
-        setTimeout(function() {
-            t.builder.ok(
-                t.iframe.contentWindow.Wikiwyg,
-                'Double click starts wikiwyg'
-            );
-            t.builder.ok(
-                t.$("div#st-display-mode-container").is(":hidden"),
-                'Display is hidden after doubleclick to edit'
-            );
+        t.callNextStep(5000);
+    },
 
-            t.builder.ok(
-                t.$("div#st-edit-mode-view").is(":visible"),
-                'Editor is now visible'
-            );
+    function() {
+        t.ok(
+            t.win.Wikiwyg,
+            'Double click starts wikiwyg'
+        );
 
-            t.endAsync();
-        }, 1500);
+        t.ok(
+            t.$("div#st-display-mode-container").is(":hidden"),
+            'Display is hidden after doubleclick to edit'
+        );
+
+        t.ok(
+            t.$("div#st-edit-mode-view").is(":visible"),
+            'Editor is now visible'
+        );
+
+        t.endAsync();
     }
 ]);
 

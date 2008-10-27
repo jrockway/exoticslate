efaefa9756d9ff455f20a54bbe2abcf843cef894
Audrey Tang <audrey.tang@socialtext.com>
2008-10-27 18:26:58 +0000

    * Modernise {bz: 1355}'s js-test, and avoid the "stack overflow" bug on IE.
    git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/trunk@10814 593ce9bc-2028-0410-bb98-c566b643a948

--- nlw/share/skin/js-test/s3/t/bz-1355.t.js
+++ nlw/share/skin/js-test/s3/t/bz-1355.t.js
@@ -8,40 +8,42 @@ t.checkRichTextSupport();
 
 t.runAsync([
     function() {
-        t.open_iframe("/admin/index.cgi?action=new_page", t.nextStep());
+        t.open_iframe("/admin/?action=new_page", t.nextStep(15000));
     },
             
     function() { 
-        setTimeout(function(){
-            t.$('#wikiwyg_button_link').click();
+        t.$('#st-newpage-pagename-edit').val('bz_1318_'+Math.random());
+        t.$('#st-newpage-pagename-edit').focus();
+        t.$('a[do=do_widget_link2_hyperlink]').click();
 
-            setTimeout(function(){
-                t.$('#st-newpage-pagename-edit').val('bz_1318_'+Math.random());
-                t.$('#st-newpage-pagename-edit').focus();
+        t.callNextStep(1500);
+    },
+            
+    function() { 
+        t.$('#web-link-text').val('Test');
+        t.$('#web-link-destination').val('http://socialtext.com/');
+        t.$('#add-a-link-form').submit();
+        
+        t.callNextStep(3000);
+    },
+            
+    function() { 
+        var href = $(
+            t.$('#st-page-editing-wysiwyg').get(0).contentWindow.document.documentElement
+        ).find('a[href=http://socialtext.com/]');
 
-                t.scrollTo(300);
+        t.ok(
+            href.length,
+            "Creating a new link on an empty page should work"
+        );
 
-                t.$('#add-web-link').click();
-                t.$('#web-link-text').val('Test');
-                t.$('#web-link-destination').val('http://socialtext.com/');
-                t.$('#add-a-link-form').submit();
+        t.$('#st-save-button-link').click();
+        t.callNextStep(3000);
 
-                setTimeout(function(){
-                    var href = $(
-                        t.$('#st-page-editing-wysiwyg').get(0).contentWindow.document.documentElement
-                    ).find('a[href=http://socialtext.com/]');
-
-                    t.ok(
-                        href.length,
-                        "Creating a new link on an empty page should work"
-                    );
-
-                    t.$('#st-save-button-link').click();
-
-                    t.endAsync();
-                }, 2500);
-            }, 2500);
-        }, 2500);
+    },
+            
+    function() { 
+        t.endAsync();
     }
 ]);
 

766ef7ef6dc01acb49a0d8f9907cbdceadf53c05
Audrey Tang <audrey.tang@socialtext.com>
2008-10-29 23:05:49 +0000

    * t/lib/Time/HiRes.pm: Export "sleep" for Socialtext::ApacheDaemon.
    git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/trunk@10850 593ce9bc-2028-0410-bb98-c566b643a948

--- nlw/t/lib/Time/HiRes.pm
+++ nlw/t/lib/Time/HiRes.pm
@@ -3,10 +3,11 @@ package Time::HiRes;
 use strict;
 use warnings;
 use base 'Exporter';
-our @EXPORT_OK = qw/time/;
+our @EXPORT_OK = qw/time sleep/;
 
 our $TIME = 1;
 
 sub time { return $TIME++ }
+sub sleep { sleep $_[0] }
 
 1;

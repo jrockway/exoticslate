841f9518bb7d7cbcb26ae47271b8dcd290205726
Audrey Tang <audrey.tang@socialtext.com>
2008-10-31 17:51:30 +0000

    * {bz: 1561}, part 1 of 2: Absolutify REST Link Dictionary.
    git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/trunk@10891 593ce9bc-2028-0410-bb98-c566b643a948

--- nlw/lib/Socialtext/Formatter/RESTLinkDictionary.pm
+++ nlw/lib/Socialtext/Formatter/RESTLinkDictionary.pm
@@ -8,21 +8,22 @@ use base 'Socialtext::Formatter::LinkDictionary';
 
 use Class::Field qw'field';
 
-field free      => '%{page_uri}';
-field interwiki =>
-    '/data/workspaces/%{workspace}/pages/%{page_uri}%{section}';
+use constant Prefix => '%{url_prefix}/data/workspaces/%{workspace}';
+
+field free      => Prefix.'/pages/%{page_uri}%{section}';
+field interwiki => Prefix.'/pages/%{page_uri}%{section}';
 field interwiki_edit => '';
-field search_query => '/data/workspaces/%{workspace}/pages?q=%{search_term}';
-field category_query => '/data/workspaces/%{workspace}/tags/%{category}/pages';
+field search_query => Prefix.'/pages?q=%{search_term}';
+field category_query => Prefix.'/tags/%{category}/pages';
 
 # REVIEW: What's the right count, is any count at all right?
 field recent_changes_query =>
-    '/data/workspaces/%{workspace}/pages?order=newest;count=50';
+    Prefix.'/pages?order=newest;count=50';
 
-field category => '/data/workspaces/%{workspace}/tags/%{category}/pages';
-field weblog   => '/data/workspaces/%{workspace}/tags/%{category}/pages';
-field file     => '/data/workspaces/%{workspace}/attachments/%{page_uri}:%{id}/files/%{filename}';
-field image    => '/data/workspaces/%{workspace}/attachments/%{page_uri}:%{id}/files/%{filename}';
+field category => Prefix.'/tags/%{category}/pages';
+field weblog   => Prefix.'/tags/%{category}/pages';
+field file     => Prefix.'/attachments/%{page_uri}:%{id}/files/%{filename}';
+field image    => Prefix.'/attachments/%{page_uri}:%{id}/files/%{filename}';
 
 # REVIEW: this is for http:text which is just weird so we let it ride, with
 # a slight adjustment

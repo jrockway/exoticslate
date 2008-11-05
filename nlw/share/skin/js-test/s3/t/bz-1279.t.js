54440ea0b7560ebdd3920e6d463260ddc9c938c2
Audrey Tang <audrey.tang@socialtext.com>
2008-11-05 17:51:37 +0000

    * {bz: 1600}, part 1 of 4: Fix visual test for {bz: 1279} under browser-side 404 pages.
    git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/trunk@10945 593ce9bc-2028-0410-bb98-c566b643a948

--- nlw/share/skin/js-test/s3/t/bz-1279.t.js
+++ nlw/share/skin/js-test/s3/t/bz-1279.t.js
@@ -7,23 +7,34 @@ t.plan(2);
 t.runAsync([
     function() {
         t.open_iframe("/data/workspaces/admin/attachments/admin_wiki:0-0-0/original/VeryBad", t.nextStep());
+        setTimeout(function() {
+            if (t.asyncId) {
+                t.skipAll('The browser has overridden our own 404 message');
+                t.endAsync();
+            }
+        }, 5000);
     },
             
     function() { 
-        if (t.doc && t.doc.body) {
-            t.unlike(
-                t.doc.body.innerHTML,
-                /Carp/,
-                "Invalid attachment URL should not lead to ugly error message"
-            );
+        try {
+            if (t.doc && t.doc.body) {
+                t.unlike(
+                    t.doc.body.innerHTML,
+                    /Carp/,
+                    "Invalid attachment URL should not lead to ugly error message"
+                );
 
-            t.like(
-                t.doc.body.innerHTML,
-                /VeryBad/,
-                "The error message should contain the attachment file name"
-            );
+                t.like(
+                    t.doc.body.innerHTML,
+                    /VeryBad/,
+                    "The error message should contain the attachment file name"
+                );
+            }
+            else {
+                t.skipAll('The browser has overridden our own 404 message');
+            }
         }
-        else {
+        catch (e) {
             t.skipAll('The browser has overridden our own 404 message');
         }
 

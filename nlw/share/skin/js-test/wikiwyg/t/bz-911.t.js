b741a9efd596ea3bf1741685fa0ae5b6670a3c23
Audrey Tang <audrey.tang@socialtext.com>
2008-11-09 19:02:03 +0000

    * Wikiwyg Test for {bz: 911}.
    git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/trunk@10996 593ce9bc-2028-0410-bb98-c566b643a948

---
+++ nlw/share/skin/js-test/wikiwyg/t/bz-911.t.js
@@ -0,0 +1,25 @@
+var t = new Test.Wikiwyg();
+
+t.plan(1);
+
+t.run_roundtrip('input', 'output');
+
+/* Test
+=== WAFL blocks need surrounding vertical whitespace
+--- input
+top
+
+foo {search: bar} baz
+
+bottom
+--- output
+top
+
+foo
+
+{search: bar}
+
+baz
+
+bottom
+*/

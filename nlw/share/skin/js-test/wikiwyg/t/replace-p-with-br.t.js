d55b1fe8599ba8ba00fff321748464f3f6f9a48b
Audrey Tang <audrey.tang@socialtext.com>
2008-11-22 15:24:48 +0000

    * replace-p-with-br.t.js shouldn't run under IE; also avoid contentless <div/>.
    git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/trunk@11151 593ce9bc-2028-0410-bb98-c566b643a948

--- nlw/share/skin/js-test/wikiwyg/t/replace-p-with-br.t.js
+++ nlw/share/skin/js-test/wikiwyg/t/replace-p-with-br.t.js
@@ -6,14 +6,19 @@ t.filters({
 
 t.plan(1);
 
-// XXX: The result matches /bc/ when run under editor, but not under js-test!
-t.run_is('p', 'br');
+if (jQuery.browser.msie) {
+    t.skipAll('On MSIE, replace_p_with_br is not used at all');
+}
+else {
+    t.run_is('p', 'br');
+}
 
 /* Test
 === {bz: 911}: Conver P to BR when inline div is present
 --- p
-<div class="wiki"><p>a</p><p><div/>b</p><p>c</p></div>
+<div class="wiki"><p>a</p><p><div>b</div>c</p><p>d</p></div>
+
 --- br
-<div class="wiki">a<br class="p"><br class="p"><div/>b<br class="p"><br class="p">c<br class="p"><br class="p"></div>
+<div class="wiki">a<br class="p"><br class="p"><div>b</div>c<br class="p"><br class="p">d<br class="p"><br class="p"></div>
 
 */

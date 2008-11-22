aea67c5c0ac928dbae9ac15ccf9c3270afe691c9
Audrey Tang <audrey.tang@socialtext.com>
2008-11-22 17:08:18 +0000

    * We should assert vertical spaces around divs after all; adjust tests.
    git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/trunk@11154 593ce9bc-2028-0410-bb98-c566b643a948

--- nlw/share/skin/js-test/wikiwyg/t/bz-911.t.js
+++ nlw/share/skin/js-test/wikiwyg/t/bz-911.t.js
@@ -1,15 +1,54 @@
 var t = new Test.Wikiwyg();
 
-t.plan(1);
+t.plan(2);
 
-t.run_roundtrip('input');
+if (jQuery.browser.safari) {
+    t.skipAll('Skip roundtrip tests on Safari');
+}
+else {
+    t.run_roundtrip('input', 'output');
+}
 
 /* Test
-=== Inline WAFL blocks should not add extra vertical whitespace
+=== Inline WAFL blocks should add extra vertical whitespace around it
 --- input
 top line
 
 middle {search: foo} line
+
+bottom line
+
+--- output
+top line
+
+middle
+
+{search: foo}
+
+line
+
+bottom line
+
+=== After adding vertical whitespace, it should roundtrip correctly
+--- input
+top line
+
+middle
+
+{search: foo}
+
+line
+
+bottom line
+
+--- output
+top line
+
+middle
+
+{search: foo}
+
+line
 
 bottom line
 

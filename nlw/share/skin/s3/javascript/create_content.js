5c16fb14a3c24d31a2dc645dea34c210a02878f3
Orlando Vazquez <ovazquez@gmail.com>
2008-11-28 02:27:02 +0000

    Implement enabling/disabling of sections in the Create Content lightbox dialog.
    git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/trunk@11258 593ce9bc-2028-0410-bb98-c566b643a948

--- nlw/share/skin/s3/javascript/create_content.js
+++ nlw/share/skin/s3/javascript/create_content.js
@@ -8,10 +8,11 @@ proto.createContentLightbox = function () {
     this.sel = '#st-create-content-lightbox';
     this.show(
         false,
-        function () {
-        }
+        self.callback
     );
 }
+
+proto.callback = function () { }
 
 proto.process = function (template) {
     Socialtext.loc = loc;
@@ -30,4 +31,51 @@ proto.show = function (do_redirect, callback) {
 
     // Clear errors from the previous time around: {bz: 1039}
     jQuery(self.sel + ' .error').html('');
+
+    // toggle enabling and disabling mutually exclusive options
+    var enable_and_disable = function(enable_id, disable_id) {
+        jQuery('#st-create-content-lightbox ' + disable_id + ' input')
+            .attr('disabled', 'disabled');
+        jQuery('#st-create-content-lightbox ' + enable_id + ' input')
+            .attr('disabled', '');
+    }
+    var enable_blank = function() {
+        enable_and_disable('#blank-option', '#template-option');
+    }
+    var enable_from_template = function() {
+        enable_and_disable('#template-option', '#blank-option');
+    }
+    var enable_use_template = function() {
+        enable_and_disable('#template-option-from-template',
+            '#template-option-from-existing-page');
+    }
+    var enable_copy_existing = function() {
+        enable_and_disable('#template-option-from-existing-page',
+            '#template-option-from-template');
+    }
+    jQuery('#st-create-content-lightbox #blank-radio')
+    .unbind('click').click(function () {
+        enable_blank();
+    });
+    jQuery('#st-create-content-lightbox #template-radio')
+    .unbind('click').click(function () {
+        enable_from_template();
+        // enable the section for whichever radio is selected
+        if (jQuery('#st-create-content-lightbox #from-template-radio')
+            .is(':checked')) {
+            enable_use_template();
+        } else {
+            enable_copy_existing();
+        }
+    });
+    jQuery('#st-create-content-lightbox #from-template-radio')
+    .unbind('click').click(function () {
+        enable_use_template();
+    });
+    jQuery('#st-create-content-lightbox #from-existing-page-radio')
+    .unbind('click').click(function () {
+        enable_copy_existing();
+    });
+
+    enable_blank();
 }

1397ede47278149c9d517bc165f810fd9b00abd5
Audrey Tang <audrey.tang@socialtext.com>
2008-12-25 12:53:06 +0000

    * Visual Test for {bz: 1874}.
    git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/trunk@11827 593ce9bc-2028-0410-bb98-c566b643a948

---
+++ nlw/share/skin/js-test/s3/t/bz-1874.t.js
@@ -0,0 +1,37 @@
+(function($) {
+
+var t = new Test.Visual();
+
+t.plan(1);
+
+var incipient = "bz_1874_" + Date.now();
+
+t.runAsync([
+    function() {
+        t.put_page({
+            workspace: 'admin',
+            page_name: "bz_1874",
+            content: "{include: [" + incipient + "]}\n",
+            callback: t.nextStep()
+        });
+    },
+
+    function() {
+        t.open_iframe(
+            "/lite/page/admin/bz_1874",
+            t.nextStep()
+        );
+    },
+
+    function() { 
+        t.like(
+            $('a.wiki-include-edit-link', t.doc).attr('href'),
+            /\/lite\//,
+            "The edit link goes to /lite/ when viewed inside /lite/"
+        );
+
+        t.endAsync();
+    }
+]);
+
+})(jQuery);

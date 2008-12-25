8a4226af099b8145d9b5e77757ab0c7eb7a6634d
Audrey Tang <audrey.tang@socialtext.com>
2008-12-25 15:10:52 +0000

    * Visual Test for {bz: 1838}.
    git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/trunk@11829 593ce9bc-2028-0410-bb98-c566b643a948

---
+++ nlw/share/skin/js-test/s3/t/bz-1838.t.js
@@ -0,0 +1,60 @@
+(function($) {
+
+var t = new Test.Visual();
+
+t.plan(2);
+
+var incipient = "bz_1838_" + Date.now();
+
+t.runAsync([
+    function() {
+        t.put_page({
+            workspace: 'admin',
+            page_name: "bz_1838",
+            content: "{include: [" + incipient + "]}\n",
+            callback: t.nextStep()
+        });
+    },
+
+    function() {
+        t.open_iframe(
+            "/lite/page/admin/bz_1838",
+            t.nextStep()
+        );
+    },
+
+    function() { 
+        t.open_iframe(
+            $('a.wiki-include-edit-link', t.doc).attr('href'),
+            t.nextStep()
+        );
+    },
+
+    function() { 
+        $('#edit_textarea', t.doc).val('fnord');
+        $('form', t.doc).submit();
+
+        t.open_iframe(
+            "/admin/?" + incipient,
+            t.nextStep()
+        );
+    },
+
+    function() { 
+        t.is(
+            t.$('#st-page-titletext').text().replace(/\s/g, ''),
+            incipient,
+            "Page name for incipient pages created from /lite/ stays the same in full view"
+        );
+
+        t.is(
+            t.$('#st-page-content').text().replace(/\s/g, ''),
+            'fnord',
+            "Page text for incipient pages created from /lite/ stays the same in full view"
+        );
+
+        t.endAsync();
+    }
+]);
+
+})(jQuery);

84bee21530f914ca4deccb05b2f9b1df2474363e
Audrey Tang <audrey.tang@socialtext.com>
2008-12-28 08:44:26 +0000

    * More doCreatePage().
    git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/trunk@11866 593ce9bc-2028-0410-bb98-c566b643a948

--- nlw/share/skin/js-test/s3/t/bz-1116.t.js
+++ nlw/share/skin/js-test/s3/t/bz-1116.t.js
@@ -5,22 +5,7 @@ var t = new Test.Visual();
 t.plan(1);
 
 t.runAsync([
-    function() {
-        t.put_page({
-            workspace: 'admin',
-            page_name: "bz_1116",
-            content: "test\n",
-            callback: t.nextStep()
-        });
-    },
-
-    function() {
-        t.open_iframe(
-            "/admin/index.cgi?bz_1116",
-            t.nextStep()
-        );
-    },
-
+    t.doCreatePage("test\n"),
     t.doWikitextEdit(),
 
     function() { 

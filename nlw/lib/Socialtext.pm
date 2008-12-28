9f2cc4d3549adc9fe24b92572b3fa974df1a9242
Ken Pier <ken.pier@socialtext.com>
2008-12-28 20:27:00 +0000

    Restore correct version number
    git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/trunk@11871 593ce9bc-2028-0410-bb98-c566b643a948

--- nlw/lib/Socialtext.pm
+++ nlw/lib/Socialtext.pm
@@ -15,7 +15,7 @@ use Socialtext::Authz;
 use Socialtext::Permission 'ST_READ_PERM';
 use Socialtext::Validate qw( validate SCALAR_TYPE USER_TYPE WORKSPACE_TYPE );
 
-our $VERSION = '3.2.0.1';
+our $VERSION = '3.2.1.0';
 
 const product_version => $VERSION;
 field using_debug => 0;

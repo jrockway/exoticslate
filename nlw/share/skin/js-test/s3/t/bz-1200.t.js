2cef9d92377f66686ebbfe89f58e567c49e657d1
Audrey Tang <audrey.tang@socialtext.com>
2008-12-30 15:58:44 +0000

    * Refactor bz-1200.t.js to use doCreatePage.
    git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/trunk@11908 593ce9bc-2028-0410-bb98-c566b643a948

--- nlw/share/skin/js-test/s3/t/bz-1200.t.js
+++ nlw/share/skin/js-test/s3/t/bz-1200.t.js
@@ -1,25 +1,13 @@
-(function($) {
-
 var t = new Test.Visual();
 
 t.plan(2);
 
 t.runAsync([
-    function() {
-        t.put_page({
-            workspace: 'admin',
-            page_name: "bz_1200",
-            content: "{toc:}\n\n"
-                   + '^^ "Foo"<http://foo.com>'
-                   + "\n\n",
-            callback: t.nextStep()
-        });
-    },
+    t.doCreatePage(
+        "{toc:}\n\n" +
+        '^^ "Foo"<http://foo.com>\n\n'
+    ),
 
-    function() {
-        t.open_iframe("/admin/index.cgi?bz_1200", t.nextStep());
-    },
-            
     function() { 
         t.is(
             t.$('div.wafl_box a').length,
@@ -36,5 +24,3 @@ t.runAsync([
         t.endAsync();
     }
 ]);
-
-})(jQuery);

484d0fb633eb7c7fc3990449569d2b2ac9fe6537
Audrey Tang <audrey.tang@socialtext.com>
2008-12-30 17:46:46 +0000

    * Made {bz: 1544}'s Visual Test speedier and more robust.
    git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/trunk@11924 593ce9bc-2028-0410-bb98-c566b643a948

--- nlw/share/skin/js-test/s3/t/bz-1544.t.js
+++ nlw/share/skin/js-test/s3/t/bz-1544.t.js
@@ -1,17 +1,15 @@
-(function($) {
-
 var t = new Test.Visual();
 
 t.plan(1);
 
 t.runAsync([
-    function() {
-        t.open_iframe("/admin/index.cgi?admin_wiki", t.nextStep());
-    },
-            
+    t.doCreatePage(),
+
     function() { 
         t.$('#st-comment-button-link').click();
-        t.callNextStep(5000);
+        t.poll(function(){
+            return t.$('div.comment').is(':visible');
+        }, t.nextStep(2000));
     },
             
     function() { 
@@ -29,5 +27,3 @@ t.runAsync([
         t.endAsync();
     }
 ]);
-
-})(jQuery);

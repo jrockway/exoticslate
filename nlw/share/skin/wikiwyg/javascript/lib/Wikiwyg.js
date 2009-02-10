9785ad9912960d461f53b68590e454a3fb2ee054
Orlando Vazquez <ovazquez@gmail.com>
2009-02-09 18:28:51 -0800

    Propagate the 'Signal this edit' checkbox value to the backend

--- nlw/share/skin/wikiwyg/javascript/lib/Wikiwyg.js
+++ nlw/share/skin/wikiwyg/javascript/lib/Wikiwyg.js
@@ -653,6 +653,9 @@ proto.saveChanges = function() {
     jQuery('#st-page-editing-summary')
         .val(this.edit_summary());
 
+    jQuery('#st-page-editing-signal-summary')
+        .val(jQuery('#st-edit-summary-signal-checkbox')[0].checked ? '1' : '0');
+
     var submit_changes = function(wikitext) {
         /*
         if ( Wikiwyg.is_safari ) {

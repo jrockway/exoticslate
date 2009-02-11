56b542c1305f9914e9922053899548005802ee6f
Luke Closs <gravatar@5thplane.com>
2009-02-11 10:54:38 -0800

    bugfix version bump

--- nlw/lib/Socialtext.pm
+++ nlw/lib/Socialtext.pm
@@ -15,7 +15,7 @@ use Socialtext::Authz;
 use Socialtext::Permission 'ST_READ_PERM';
 use Socialtext::Validate qw( validate SCALAR_TYPE USER_TYPE WORKSPACE_TYPE );
 
-our $VERSION = '3.3.0.7';
+our $VERSION = '3.3.0.8';
 
 const product_version => $VERSION;
 field using_debug => 0;

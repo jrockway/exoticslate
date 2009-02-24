72fbde5dc04f3e8de0f41ccb089973916556e5a7
Kevin Jones <kevin.jones@socialtext.com>
2009-02-24 12:11:58 -0800

    Allow callbacks in the simple lightbox

--- nlw/share/skin/s3/javascript/lightbox/simple.js
+++ nlw/share/skin/s3/javascript/lightbox/simple.js
@@ -3,22 +3,26 @@ var ST = window.ST = window.ST || {};
 
 ST.SimpleLightbox = function () {};
 ST.SimpleLightbox.prototype = {
-    show: function (title, msg) {
+    show: function (title, msg, cb) {
         $.showLightbox({
             html: Jemplate.process('simple.tt2', { loc: loc, msg: msg, title: title }),
             close: '#simple-lightbox .close'
         });
+        
+        if (cb) {
+            $('#simple-lightbox .close').unbind('click').click(cb);
+        }
     }
 }
 
 })(jQuery);
 
-function errorLightbox(error) {
+function errorLightbox(error, cb) {
     var lb = new ST.SimpleLightbox;
-    lb.show(loc('Error'), error);
+    lb.show(loc('Error'), error, cb);
 }
 
-function successLightbox(msg) {
+function successLightbox(msg, cb) {
     var lb = new ST.SimpleLightbox;
-    lb.show(loc('Success'), msg);
+    lb.show(loc('Success'), msg, cb);
 }

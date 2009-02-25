0fbbd29891f90bc73ee2f006267686b3208cff4f
Luke Closs <gravatar@5thplane.com>
2009-02-25 13:38:46 -0800

    make the proxy used for soap requests

--- nlw/lib/Socialtext/InitHandler.pm
+++ nlw/lib/Socialtext/InitHandler.pm
@@ -40,6 +40,9 @@ sub handler {
         $Socialtext::User::Cache::Enabled = 1;
     }
 
+    if (my $proxy = Socialtext::AppConfig->web_services_proxy) {
+        $ENV{http_proxy} = $ENV{HTTP_proxy} = $proxy;
+    }
 }
 
 sub _regen_combined_js {

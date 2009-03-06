548a5c18bba216f460a50a9282bd90329ba5ecef
Brandon Noard <brandon.noard@socialtext.com>
2009-03-06 14:18:42 -0800

    Add HTML output for users in the HTML emitter. Do some code cleanup and refactor.

--- nlw/lib/Socialtext/WikiText/Emitter/Messages/HTML.pm
+++ nlw/lib/Socialtext/WikiText/Emitter/Messages/HTML.pm
@@ -20,7 +20,22 @@ sub init {
 sub insert {
     my $self = shift;
     my $ast = shift;
-    $self->{output} .= $ast->{output} || '';
+    my $output = '';
+
+    if (not(defined($ast->{wafl_type}))) {
+        $output = $ast->{output} || '';
+    }
+    elsif ( $ast->{wafl_type} eq 'link' ) {
+        $output = qq{<a href="/$ast->{workspace_id}/index.cgi?$ast->{page_id}">$ast->{text}</a>};
+    }
+    elsif ( $ast->{wafl_type} eq 'user' ) {
+        $output = $self->user_html($ast);
+    }
+    else {
+        $output = qq{{$ast->{wafl_type}: not supported yet, fool}};
+    }
+
+    $self->{output} .= $output;
 }
 
 my $markup = {
@@ -28,32 +43,48 @@ my $markup = {
     'i' => ['<i>', '</i>'],
     'del' => ['<del>', '</del>'],
 };
+
+sub user_html {
+    my $self = shift;
+    my $ast = shift;
+    my $userid = $ast->{user_string};
+    my $viewer = $self->{callbacks}{viewer};
+
+    my $user = eval { Socialtext::User->Resolve($userid) };
+    unless ($user) {
+        return "Unknown Person";
+    }
+
+    unless ($viewer && $user->profile_is_visible_to($viewer)) {
+        return $user->guess_real_name;
+    }
+    else {
+        return '<a href="/?profile=' . $user->user_id 
+            . '">' . $user->guess_real_name . '</a>';
+    }
+}
+
 sub begin_node {
     my $self = shift;
     my $match = shift;
-    return if ($match->{type} eq 'line');
+
     if ($match->{type} eq 'a') {
         $self->{output} .= qq{<a href="$match->{attributes}{href}">};
     }
     elsif (exists $markup->{$match->{type}}) {
         $self->{output} .= $markup->{$match->{type}}->[0];
     }
-    else {
-        $self->{output} .= " ";
-    }
 }
 
 sub end_node {
     my $self = shift;
     my $match = shift;
+
     if ($match->{type} eq 'a') {
         $self->{output} .= "</a>";
     }
     elsif (exists $markup->{$match->{type}}) {
         $self->{output} .= $markup->{$match->{type}}->[1];
-    }
-    else {
-        $self->{output} .= " ";
     }
 }
 

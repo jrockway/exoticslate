d515928c29881515df31edd4cdfecd9cecfc4694
Stash <jeremy.stashewsky@socialtext.com>
2009-03-31 12:50:12 -0700

    Add a typeahead find ReST GET for users.

--- nlw/lib/Socialtext/Rest/Users.pm
+++ nlw/lib/Socialtext/Rest/Users.pm
@@ -9,9 +9,11 @@ use Socialtext::JSON;
 use Socialtext::HTTP ':codes';
 use Socialtext::User;
 use Socialtext::Exceptions;
+use Socialtext::User::Find;
+
 use base 'Socialtext::Rest::Collection';
 
-sub allowed_methods {'GET, HEAD, PUT, POST'}
+sub allowed_methods {'GET, POST'}
 
 field errors        => [];
 
@@ -23,6 +25,10 @@ sub if_authorized {
     if ($method eq 'POST') {
         return $self->not_authorized
             unless $self->_user_is_business_admin_p;
+    }
+    elsif ($method eq 'GET') {
+        return $self->not_authorized
+            if ($self->rest->user->is_guest);
     }
     else {
         return $self->bad_method;
@@ -81,6 +87,49 @@ sub _POST_json {
         -Location => $self->full_url('/', $new_user->username()),
     );
     return '';
+}
+
+sub get_resource {
+    my $self = shift;
+    my $rest = shift;
+
+    my $limit = $self->rest->query->param('count') ||
+                $self->rest->query->param('limit') ||
+                25;
+    my $offset = $self->rest->query->param('offset') || 0;
+
+    my $filter = $self->rest->query->param('filter');
+
+    my $results = [];
+    my $f = eval { 
+        Socialtext::User::Find->new(
+            viewer => $self->rest->user,
+            limit => $limit,
+            offset => $offset,
+        )
+    };
+    if ($@) {
+        warn $@;
+        $rest->header(
+            -status => HTTP_400_Bad_Request,
+            -type   => 'text/plain'
+        );
+        return "Bad request or illegal filter options";
+    }
+
+    $results = eval {
+        $f->typeahead_find($self->rest->query->param('filter'));
+    };
+    if ($@) {
+        warn $@;
+        $rest->header(
+            -status => HTTP_400_Bad_Request,
+            -type   => 'text/plain'
+        );
+        return "Illegal filter or query error";
+    }
+
+    return $results;
 }
 
 1;
---
+++ nlw/lib/Socialtext/User/Find.pm
@@ -0,0 +1,52 @@
+package Socialtext::User::Find;
+# @COPYRIGHT@
+use Moose;
+use Socialtext::SQL qw/get_dbh sql_execute/;
+use Socialtext::String;
+use namespace::clean -except => 'meta';
+
+has viewer => (is => 'rw', isa => 'Socialtext::User', required => 1);
+has limit => (is => 'rw', isa => 'Maybe[Int]');
+has offset => (is => 'rw', isa => 'Maybe[Int]');
+
+sub typeahead_find {
+    my $self = shift;
+    my $filter = shift;
+
+    die 'Illegal Filter'
+        unless (defined $filter && length $filter);
+
+    $filter = lc Socialtext::String::trim($filter);
+    $filter =~ s/[_%]//g; # remove wildcards
+    $filter .= '%';
+    my $sql = q{
+        SELECT user_id, first_name, last_name,
+               email_address, driver_username AS username
+        FROM users
+        WHERE (
+            lower(first_name) LIKE $1 OR
+            lower(last_name) LIKE $1 OR
+            lower(email_address) LIKE $1 OR
+            lower(driver_username) LIKE $1
+        )
+        AND EXISTS (
+            SELECT 1
+            FROM account_user viewer
+            JOIN account_user found USING (account_id)
+            WHERE viewer.user_id = $2 AND found.user_id = users.user_id
+        )
+        ORDER BY last_name ASC, first_name ASC
+        LIMIT $3 OFFSET $4
+    };
+
+    #local $Socialtext::SQL::PROFILE_SQL = 1;
+    my $sth = sql_execute($sql, 
+        $filter, $self->viewer->user_id, $self->limit, $self->offset);
+
+    warn "GOT ".$sth->rows." rows";
+    my $results = $sth->fetchall_arrayref({}) || [];
+    return $results;
+}
+
+__PACKAGE__->meta->make_immutable;
+1;
